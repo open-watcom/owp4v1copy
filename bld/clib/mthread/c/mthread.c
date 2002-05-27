ca1c7246d115f1596967b93ff44c07f7ca9d58c5
Michal Necasek <N/A>
2002-05-27 08:56:45 +0100

    Temporary change to prevent programs linked against DLL runtime from crashing on exit. This is NOT a real fix! The multithread support for 32-bit OS/2 seems busted somehow, at least in the DLL runtime. Need to investigate further. Note that the memset() I removed was likely causing weird problems even on other platforms besides OS/2.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 17492]

--- bld/clib/mthread/c/mthread.c
+++ bld/clib/mthread/c/mthread.c
@@ -24,8 +24,7 @@
 *
 *  ========================================================================
 *
-* Description:  WHEN YOU FIGURE OUT WHAT THIS FILE DOES, PLEASE
-*               DESCRIBE IT HERE!
+* Description:  Core functions of the C runtime multithread support.
 *
 ****************************************************************************/
 
@@ -419,8 +418,14 @@ void __FreeInitThreadData( thread_data *tdata )
 /******************************************************/
 {
     if( tdata != NULL ) {
-        memset( tdata, 0xdeaddead, __ThreadDataSize );
-        if( tdata->__allocated == 1 ) lib_free( tdata );
+        int     alloced = tdata->__allocated;
+        // We need to check tdata->__allocated first and _then_
+        // overwrite tdata, not the other way round. Note that
+        // 0xDEADDEAD ends up as 0xADADADAD pattern in memory
+        // because memset() only does chars.
+//      memset( tdata, 0xdeaddead, tdata->__data_size );
+        if( alloced )
+            lib_free( tdata );
     }
 }
 
