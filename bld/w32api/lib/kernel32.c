8dc5b37a557ca131b85c4f388879f2755f4d4765
Bart Oldeman <N/A>
2002-07-19 13:36:31 +0100

    Compile .c files too and use #pragma in kernel32.c.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 18609]

--- bld/w32api/lib/kernel32.c
+++ bld/w32api/lib/kernel32.c
@@ -25,6 +25,31 @@ void* GetFiberData(void)
     return ret;
 }
 
+#elif defined (__WATCOMC__)
+
+void* __GetCurrentFiber(void);
+#pragma aux __GetCurrentFiber = \
+        "mov    eax, dword ptr fs:0x10" \
+        value [eax] \
+        modify [eax];
+
+void* __GetFiberData(void);
+#pragma aux __GetFiberData = \
+        "mov    eax, dword ptr fs:0x10" \
+        "mov    eax, [eax]" \
+        value [eax] \
+        modify [eax];
+        
+void* GetCurrentFiber(void)
+{
+   return __GetCurrentFiber();
+}
+
+void* GetFiberData(void)
+{
+   return __GetFiberData();
+}
+
 #else
 
 void* GetCurrentFiber(void)
