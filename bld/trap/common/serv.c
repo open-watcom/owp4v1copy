102e4a0035b5ad805a8776c28d2fab0ee1a5cf6f
Michal Necasek <N/A>
2002-07-26 20:01:49 +0100

    Made the remote debug server banner slightly more compact.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 18822]

--- bld/trap/common/serv.c
+++ bld/trap/common/serv.c
@@ -134,7 +134,7 @@ void OpeningStatement( void )
 {
     Output( "Open Watcom " );
     Output( ServName );
-    Output( "\r\nVersion " _XXXSERV_VERSION_ "\r\n" );
+    Output( " Version " _XXXSERV_VERSION_ "\r\n" );
     Output( banner2( "1988" ) "\r\n" );
     Output( banner3 "\r\n" );
     Output( banner3a "\r\n" );
