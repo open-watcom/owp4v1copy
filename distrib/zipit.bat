032423ea3297b5ab83cf384872cab611a60de1c3
Michal Necasek <N/A>
2002-10-03 21:45:28 +0100

    Add license to every ZIP file again.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 19992]

--- distrib/zipit.bat
+++ distrib/zipit.bat
@@ -2,5 +2,5 @@
 echo Building '%1'...
 cd %relroot%\%defrel%
 comstrip %distroot%\manifest\%1 | zip %distroot%\archives\%1.zip -@ >> mkzips.log
-rem zip %distroot%\archives\%1.zip license.txt >> mkzips.log
+zip %distroot%\archives\%1.zip -j %relroot%\license.txt >> mkzips.log
 cd %distroot%
