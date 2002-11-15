aa99d65d57d75ca803ad5ce39a7fb12130b11b32
Michal Necasek <N/A>
2002-11-15 07:48:22 +0100

    Fixed COMENT type 0xa1 handling.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 20649]

--- bld/dmpobj/c/process.c
+++ bld/dmpobj/c/process.c
@@ -325,24 +325,30 @@ static int doEasyOmf( byte c_bits ) {
 
 static int doMSOmf( void ) {
 
-    byte    version;
-
-    version = GetByte();
-    IsIntel = FALSE;
-    NamePtr = RecPtr;
-    NameLen = RecLen - 3;
-    RecPtr += NameLen;
-    Output( INDENT "---- \"New OMF\" extensions present <%N> ----" CRLF );
-    if( strncmp( NamePtr, "CV", 2 ) == 0 ) {
-        Output( INDENT "Microsoft CodeView style debugging information version %b" CRLF, version );
+    if( RecLen < 5 ) {
+        Output( INDENT "Assuming CodeView style debugging information" CRLF );
         DbgStyle = DBG_CODEVIEW;
-    } else if( strncmp( NamePtr, "HL", 2 ) == 0 ) {
-        Output( INDENT "IBM HLL style debugging information version %b" CRLF, version );
-        DbgStyle = DBG_HLL;
     }
     else {
-        Output( INDENT "Unknown debugging information style" CRLF );
-        DbgStyle = DBG_UNKNOWN;
+        byte    version;
+
+        version = GetByte();
+        IsIntel = FALSE;
+        NamePtr = RecPtr;
+        NameLen = RecLen - 3;
+        RecPtr += NameLen;
+        Output( INDENT "---- \"New OMF\" extensions present ----" CRLF );
+        if( strncmp( NamePtr, "CV", 2 ) == 0 ) {
+            Output( INDENT "Microsoft CodeView style debugging information version %b" CRLF, version );
+            DbgStyle = DBG_CODEVIEW;
+        } else if( strncmp( NamePtr, "HL", 2 ) == 0 ) {
+            Output( INDENT "IBM HLL style debugging information version %b" CRLF, version );
+            DbgStyle = DBG_HLL;
+        }
+        else {
+            Output( INDENT "Unknown debugging information style" CRLF );
+            DbgStyle = DBG_UNKNOWN;
+        }
     }
 
     return( 1 );
