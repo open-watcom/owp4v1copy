4c33ba8f225267ab14aa8d89524b01be52c29202
Michal Necasek <N/A>
2002-11-27 09:07:57 +0100

    Added REXX import library and more.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 20901]

--- bld/os2api/incl32/rexxsaa.h
+++ bld/os2api/incl32/rexxsaa.h
@@ -5,6 +5,14 @@
 #ifndef __REXXSAA_H__
 #define __REXXSAA_H__
 
+#ifndef OS2_INCLUDED
+#include <os2.h>
+#endif
+
+#if (defined(__IBMC__) || defined(__WATCOMC__)) && !defined(REXX_NODEFLIB)
+#pragma library("rexx.lib")
+#endif
+
 #define RXCOMMAND    0
 #define RXSUBROUTINE 1
 #define RXFUNCTION   2
@@ -26,6 +34,52 @@
 #define RXSUBCOM_NOEMEM   1002
 #define RXSUBCOM_BADTYPE  1003
 #define RXSUBCOM_NOTINIT  1004
+
+#define RXENDLST 0
+#define RXFNC    2
+#define RXFNCCAL 1
+#define RXCMD    3
+#define RXCMDHST 1
+#define RXMSQ    4
+#define RXMSQPLL 1
+#define RXMSQPSH 2
+#define RXMSQSIZ 3
+#define RXMSQNAM 20
+#define RXSIO    5
+#define RXSIOSAY 1
+#define RXSIOTRC 2
+#define RXSIOTRD 3
+#define RXSIODTR 4
+#define RXSIOTLL 5
+#define RXHLT    7
+#define RXHLTCLR 1
+#define RXHLTTST 2
+#define RXTRC    8
+#define RXTRCTST 1
+#define RXINI    9
+#define RXINIEXT 1
+#define RXTER    10
+#define RXTEREXT 1
+#define RXNOOFEXITS 11
+
+#define RXSHV_SET   0
+#define RXSHV_FETCH 1
+#define RXSHV_DROPV 2
+#define RXSHV_SYSET 3
+#define RXSHV_SYFET 4
+#define RXSHV_SYDRO 5
+#define RXSHV_NEXTV 6
+#define RXSHV_PRIV  7
+#define RXSHV_EXIT  8
+#define RXSHV_NOAVL 144
+
+#define RXSHV_OK    0x00
+#define RXSHV_NEWV  0x01
+#define RXSHV_LVAR  0x02
+#define RXSHV_TRUNC 0x04
+#define RXSHV_BADN  0x08
+#define RXSHV_MEMFL 0x10
+#define RXSHV_BADF  0x80
 
 #define MAKERXSTRING(r,p,l) {(r).strptr=(PCH)p;(r).strlength=(ULONG)l;}
 #define RXNULLSTRING(r)     (!(r).strptr)
@@ -54,26 +108,25 @@ typedef struct _RXSYSEXIT {
     LONG sysexit_code;
 } RXSYSEXIT, *PRXSYSEXIT;
 
-typedef ULONG RexxSubcomHandler(PRXSTRING, PUSHORT, PRXSTRING);
-typedef ULONG RexxFunctionHandler(PUCHAR, ULONG, PRXSTRING, PSZ, PRXSTRING);
+typedef ULONG RexxSubcomHandler(PRXSTRING,PUSHORT,PRXSTRING);
+typedef ULONG RexxFunctionHandler(PUCHAR,ULONG,PRXSTRING,PSZ,PRXSTRING);
 
+APIRET APIENTRY RexxDeregisterExit(PCSZ,PCSZ);
 APIRET APIENTRY RexxDeregisterFunction(PCSZ);
-APIRET APIENTRY RexxDeregisterSubcom(PCSZ, PCSZ);
+APIRET APIENTRY RexxDeregisterSubcom(PCSZ,PCSZ);
 APIRET APIENTRY RexxQueryFunction(PCSZ);
-APIRET APIENTRY RexxQuerySubcom(PCSZ, PCSZ, PUSHORT, PUCHAR);
-APIRET APIENTRY RexxRegisterFunctionDll(PCSZ, PCSZ, PCSZ);
-APIRET APIENTRY RexxRegisterFunctionExe(PCSZ, PFN);
+APIRET APIENTRY RexxQuerySubcom(PCSZ,PCSZ,PUSHORT,PUCHAR);
+APIRET APIENTRY RexxRegisterExitExe(PCSZ,PFN,PUCHAR);
+APIRET APIENTRY RexxRegisterFunctionDll(PCSZ,PCSZ,PCSZ);
+APIRET APIENTRY RexxRegisterFunctionExe(PCSZ,PFN);
 
-APIRET APIENTRY RexxRegisterSubcomDll(PCSZ, PCSZ, PCSZ, PUCHAR, ULONG);
-APIRET APIENTRY RexxRegisterSubcomExe(PCSZ, PFN, PUCHAR);
-APIRET APIENTRY RexxResetTrace(PID, TID);
-APIRET APIENTRY RexxSetHalt(PID, TID);
-APIRET APIENTRY RexxSetTrace(PID, TID);
+APIRET APIENTRY RexxRegisterSubcomDll(PCSZ,PCSZ,PCSZ,PUCHAR,ULONG);
+APIRET APIENTRY RexxRegisterSubcomExe(PCSZ,PFN,PUCHAR);
+APIRET APIENTRY RexxResetTrace(PID,TID);
+APIRET APIENTRY RexxSetHalt(PID,TID);
+APIRET APIENTRY RexxSetTrace(PID,TID);
 
-LONG   APIENTRY RexxStart(LONG, PRXSTRING, PCSZ, PRXSTRING,
-                   PCSZ, LONG, PRXSYSEXIT, PSHORT, PRXSTRING);
+LONG   APIENTRY RexxStart(LONG,PRXSTRING,PCSZ,PRXSTRING,PCSZ,LONG,PRXSYSEXIT,PSHORT,PRXSTRING);
 APIRET APIENTRY RexxVariablePool(PSHVBLOCK);
 
 #endif
-
-
