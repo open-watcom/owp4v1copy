81d61a7eb7e25ffbd4991a20aca3fdee20e3213e
Kendall Bennett <N/A>
2002-12-20 23:06:26 +0100

    Updates to Open Watcom zipup batch file.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 21479]

--- zipup.bat
+++ zipup.bat
@@ -1,7 +1,7 @@
 @echo on
 pushd
-set LABEL=open_watcom_0.8.4
-set PREFIX=open_watcom_0.8.4
+set LABEL=open_watcom_0.9.5
+set PREFIX=open_watcom_0.9.5
 set P4OPT=-f
 set ARCHIVES=c:\archives
 cdd %ARCHIVES%
