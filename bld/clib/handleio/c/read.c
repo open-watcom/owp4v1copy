c56df13b0609fea210d09ffbdb64bd20d49e1953
Jiri Malak <N/A>
2003-01-10 00:46:38 +0100

    Pointer correction by cast.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 21742]

--- bld/clib/handleio/c/read.c
+++ bld/clib/handleio/c/read.c
@@ -224,7 +224,7 @@ _WCRTLINK int read( int handle, void *buffer, unsigned len )
         if( rc != readamt ) return( total );
 
         len -= readamt;
-        buffer += readamt;
+        (char*)buffer += readamt;
     }
     return( total );
 
