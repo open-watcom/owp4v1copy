b00d5e517192fbee24d85da9c8cf74258e0319ab
Bart Oldeman <N/A>
2003-01-16 01:20:31 +0100

    Fix typo.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 21897]

--- bld/clib/memory/c/fmemcmp.c
+++ bld/clib/memory/c/fmemcmp.c
@@ -41,7 +41,7 @@ _WCRTLINK int _fmemcmp( const void _WCFAR *v1,
         return( _inline__fmemcmp( v1, v2, len ) );
 #else
         const unsigned char _WCFAR *s1 = v1;
-        const unsigned char _WCFAR *s1 = v2;
+        const unsigned char _WCFAR *s2 = v2;
         for( ; len; --len )  {
             if( *s1 != *s2 ) return( *s1 - *s2 );
             ++s1;
