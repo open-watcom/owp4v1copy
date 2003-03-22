1a60dcd31319a62249483b1b2621f3bb9ebbe2e5
Jiri Malak <N/A>
2003-03-22 16:29:46 +0100

    Postscript internal WDEBUG documentation.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 22846]

---
+++ bld/wv/doc/ps.bat
@@ -0,0 +1,6 @@
+@set gmllib=%doc_root%\gml\syslib\
+@set gmlinc=.\
+@%gml_path%\wgml dbg (file ps.opt line out dbg
+@%gml_path%\wgml ovl (file ps.opt line out ovl
+@%gml_path%\wgml trap (file ps.opt line out trap
+@set gmllib=
