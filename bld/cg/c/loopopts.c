186be0ae17c26b5f552986d9f33b86623ffff252
Bart Oldeman <N/A>
2003-03-26 03:41:25 +0100

    Fix -ol overflow comparison loop optimizer bug.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 22898]

--- bld/cg/c/loopopts.c
+++ bld/cg/c/loopopts.c
@@ -2554,7 +2554,6 @@ static  bool    DangerousTypeChange( induction *var, induction *other ) {
 
     invariant   *invar;
 
-    if( var->type_class == other->type_class ) return( FALSE );
     if( PointerOk( other->name ) ) return( FALSE );
     invar = other->invar;
     while( invar != NULL ) {
