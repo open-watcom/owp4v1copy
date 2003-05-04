a018ee2e4e8939a099a8d76846cac01ea14d7460
Bart Oldeman <N/A>
2003-05-04 19:10:10 +0100

    Add common include file for the GNU makefiles.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 23584]

---
+++ bld/build/mif/master.mk
@@ -0,0 +1,22 @@
+SDK_DIR = $(DEVDIR)/sdk
+SDK_DIR = $(DEVDIR)/sdk
+RC_DIR = $(SDK_DIR)/rc/rc
+WRES_DIR = $(SDK_DIR)/rc/wres
+WCLIB_DIR = $(DEVDIR)/clib
+WATCOMH = $(DEVDIR)/watcom/h
+H_DIR = $(DEVDIR)/watcom/h
+CPP_DIR = $(DEVDIR)/cpp
+DWARF_DIR = $(DEVDIR)/dwarf
+DIP_DIR = $(DEVDIR)/dip
+LIB_MISC_DIR = $(DEVDIR)/lib_misc
+ORL_DIR = $(DEVDIR)/orl
+CPPFLAGS = $(addprefix -I,$(INCDIRS)) $(DEFS)
+LIBS = $(addprefix -L,$(LIBDIRS))
+LDLIBS = $(LIBS)
+
+CFLAGS = -funsigned-char -Wall -g -O2
+CPPFLAGS = $(addprefix -I,$(INCDIRS)) $(DEFS)
+DEFS = -D__386__ -D__LINUX__ -DLINUX -D__UNIX__ -DUNIX -D_WCUNALIGNED="__attribute__((packed))" -D_LINKER=_WLINK -D__FLAT__ -Uunix -DBOOTSTRAP
+
+$(OBJDIR)/%.o: %.c
+	$(CC) -c $(CFLAGS) $(CPPFLAGS) -o $@ $<
