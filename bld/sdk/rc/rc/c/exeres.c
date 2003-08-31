fcd81a1ed9144c94d18bd0f771a0163ca5f5edf3
ErnestK <a@b>
2003-08-31 11:07:19 +0100

    Corrected a compiler warning.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 24563]

--- bld/sdk/rc/rc/c/exeres.c
+++ bld/sdk/rc/rc/c/exeres.c
@@ -275,6 +275,7 @@ extern int CopyResources( uint_16 sect2mask, uint_16 sect2bits, bool sect2 )
 
     /* walk through the WRes directory */
     wind = WResFirstResource( dir );
+    exe_type = NULL;
     while (!WResIsEmptyWindow( wind )) {
         if (WResIsFirstResOfType( wind )) {
             exe_type = findExeTypeRecord( restab, WResGetTypeInfo( wind ) );
