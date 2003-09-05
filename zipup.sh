9550ac8eaa3f12a4513a6f0b2a23b207035cd7c6
Kendall Bennett <N/A>
2003-09-05 20:09:10 +0100

    Added Linux zipup batch files.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 24603]

---
+++ zipup.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+RELEASE=1.1.7
+LABEL=open_watcom_devel_$RELEASE
+PREFIX=open_watcom_devel_$RELEASE
+P4OPT=-f
+P4PORT=-pp4.scitechsoft.com:3488
+ARCHIVES=/archives
+cd $ARCHIVES
+rm -f *.bz2
+
+#############################################################################
+# Archive all Open Watcom source files
+rm -rf $ACHIVES/ow_devel_src
+p4 $P4PORT -uanonymous -Panonymous -cOPENWATCOM_DEVEL_SRC sync $P4OPT @$LABEL
+cd $ARCHIVES/ow_devel_src
+tar -cv * | bzip2 -9 > ../$PREFIX-src.tar.bz2
+
+cd ~/ow
+
