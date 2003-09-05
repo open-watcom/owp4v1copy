f7cf22199e4c728b938ab1e48665b46742f67172
Kendall Bennett <N/A>
2003-09-05 19:38:40 +0100

    Updates to Open Watcom zip batch file
    [git-p4: depot-paths = "//depot/openwatcom/": change = 24601]

--- zipup.bat
+++ zipup.bat
@@ -1,7 +1,7 @@
-@echo on
+@echo off
 pushd
-set LABEL=open_watcom_1.0.1
-set PREFIX=open_watcom_1.0.1
+set LABEL=open_watcom_devel_1.1.7
+set PREFIX=open_watcom_devel_1.1.7
 set P4OPT=-f
 set ARCHIVES=c:\archives
 cdd %ARCHIVES%
@@ -9,17 +9,10 @@ del /q *.zip >& NUL
 
 rem ##########################################################################
 rem ZIP all Open Watcom source files
-rm -rf %ARCHIVES%\ow_src
-p4 -uanonymous -Panonymous -cOPENWATCOM_SRC sync %P4OPT% @%LABEL%
-cd %ARCHIVES%\ow_src
+rm -rf %ARCHIVES%\ow_devel_src
+p4 -uanonymous -Panonymous -cOPENWATCOM_DEVEL_SRC sync %P4OPT% @%LABEL%
+cd %ARCHIVES%\ow_devel_src
 zip -r ..\%PREFIX%-src.zip *
-
-rem ##########################################################################
-rem ZIP all Open Watcom documentation source files
-rm -rf %ARCHIVES%\ow_docs
-p4 -uanonymous -Panonymous -cOPENWATCOM_DOCS sync %P4OPT% @%LABEL%
-cd %ARCHIVES%\ow_docs
-zip -r ..\%PREFIX%-docs.zip *
 
 popd
 
