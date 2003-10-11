a7e8917d674ffb45089fd0829df29efb6225f15f
Jiri Malak <N/A>
2003-10-11 22:14:46 +0100

    Minor change.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 24771]

--- bld/sdk/misc/ctl3d.h
+++ bld/sdk/misc/ctl3d.h
@@ -24,20 +24,10 @@
 *
 *  ========================================================================
 *
-* Description:  WHEN YOU FIGURE OUT WHAT THIS FILE DOES, PLEASE
-*               DESCRIBE IT HERE!
+* Description:  Adds 3d effects to Windows controls
 *
 ****************************************************************************/
 
-
-/*-----------------------------------------------------------------------
-|       CTL3D.DLL
-|
-|       Adds 3d effects to Windows controls
-|
-|       See ctl3d.doc for info
-|
------------------------------------------------------------------------*/
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -83,6 +73,7 @@ VOID WINAPI Ctl3dWinIniChange(void);
 //end DBCS
 
 #else
+
 #define Ctl3dSubclassDlg(a, b)
 #define Ctl3dSubclassDlgEx(a, b)
 #define Ctl3dGetVer()
@@ -94,23 +85,24 @@ VOID WINAPI Ctl3dWinIniChange(void);
 #define Ctl3dAutoSubclass( a )
 #define Ctl3dRegister( a )
 #define Ctl3dUnregister( a )
+
 #endif
 
 /* Ctl3dSubclassDlg3d flags */
 #define CTL3D_BUTTONS           0x0001
 #define CTL3D_LISTBOXES         0x0002
-#define CTL3D_EDITS                     0x0004
-#define CTL3D_COMBOS                    0x0008
+#define CTL3D_EDITS             0x0004
+#define CTL3D_COMBOS            0x0008
 #define CTL3D_STATICTEXTS       0x0010
 #define CTL3D_STATICFRAMES      0x0020
 
 #define CTL3D_NODLGWINDOW       0x00010000
-#define CTL3D_ALL                               0xffff
+#define CTL3D_ALL               0xffff
 
 #define WM_DLGBORDER (WM_USER+3567)
 /* WM_DLGBORDER *(int FAR *)lParam return codes */
 #define CTL3D_NOBORDER          0
-#define CTL3D_BORDER                    1
+#define CTL3D_BORDER            1
 
 #define WM_DLGSUBCLASS (WM_USER+3568)
 /* WM_DLGSUBCLASS *(int FAR *)lParam return codes */
@@ -118,7 +110,7 @@ VOID WINAPI Ctl3dWinIniChange(void);
 #define CTL3D_SUBCLASS          1
 
 /* Resource ID for 3dcheck.bmp (for .lib version of ctl3d) */
-#define CTL3D_3DCHECK 26567
+#define CTL3D_3DCHECK           26567
 
 
 #ifdef __cplusplus
