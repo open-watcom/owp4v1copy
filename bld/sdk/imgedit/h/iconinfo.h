312edc9d40da474740916499aa8b82379bcc5362
Roald Ribe <N/A>
2004-02-02 08:09:01 +0100

    Added support for 64x64 & 96x96 icons in 2, 16 and 256 colors.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 25842]

--- bld/sdk/imgedit/h/iconinfo.h
+++ bld/sdk/imgedit/h/iconinfo.h
@@ -34,7 +34,8 @@
 #define DELETE_ICON_OP  1
 #define ADD_ICON_OP     2
 #define SEL_ICON_OP     3
-#define NUM_OF_ICONS    10
+//#define NUM_OF_ICONS    10
+#define NUM_OF_ICONS   16
 
 typedef struct {
     char        *text;
