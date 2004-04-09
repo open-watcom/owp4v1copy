bb372b532cfa364f17e4dae4dac6589c18e8799b
Michal Necasek <N/A>
2004-04-09 01:09:14 +0100

    The Cat(N)StrToVec functions seem to be superfluous - if the current code works, all calls to Cat(N)StrToVec can be replaced.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 26219]

--- bld/wmake/c/mvecstr.c
+++ bld/wmake/c/mvecstr.c
@@ -24,10 +24,10 @@
 *
 *  ========================================================================
 *
-* Description:  WHEN YOU FIGURE OUT WHAT THIS FILE DOES, PLEASE
-*               DESCRIBE IT HERE!
+* Description:  Functions for "text vector" handling.
 *
 ****************************************************************************/
+
 
 #include <string.h>
 
@@ -295,8 +295,8 @@ extern void WriteVec( VECSTR vec, const char *text )
 }
 
 
-extern void WriteNVec( VECSTR vec, const char *text, size_t len)
-/**************************************************
+extern void WriteNVec( VECSTR vec, const char *text, size_t len )
+/****************************************************************
  * write vector with input string length len
  */
 {
@@ -306,6 +306,7 @@ extern void WriteNVec( VECSTR vec, const char *text, size_t len)
         cpyTxt( (OURPTR) vec, text, len);
     }
 }
+
 
 extern void CatVec( VECSTR dest, VECSTR src )
 /********************************************
@@ -326,35 +327,22 @@ extern void CatVec( VECSTR dest, VECSTR src )
     freeVec = (OURPTR)src;
 }
 
+// TODO: If these functions work the way I think they do, all calls to
+//       Cat(N)StrToVec should be just replaced with Write(N)Vec - MN
 
-extern void CatStrToVec( VECSTR dest, const char* str) {
-/**************************************************
+extern void CatStrToVec( VECSTR dest, const char* str )
+/******************************************************
  *  Concatenate the whole string ended by nullchar to the end of dest
- *  Same with WriteVec but adds handling of string length = 0
  */
-
-    size_t length;
-    VECSTR temp;
-
-    length = strlen (str);
-    if (length > 0 ) {
-
-        temp = StartVec();
-        WriteVec(temp,str);
-        CatVec(dest,temp);
-    }
-
+{
+    WriteVec( dest, str );
 }
 
 
-/* Concatenate a string to the end of dest with length length */
-extern void CatNStrToVec( VECSTR dest, const char* str, size_t length) {
-
-    VECSTR temp;
-
-    if (length > 0 ) {
-        temp = StartVec();
-        WriteNVec(temp,str,length);
-        CatVec(dest,temp);
-    }
+extern void CatNStrToVec( VECSTR dest, const char* str, size_t length )
+/**********************************************************************
+ * Concatenate a string to the end of 'dest' with length 'length'
+ */
+{
+    WriteNVec( dest, str, length );
 }
