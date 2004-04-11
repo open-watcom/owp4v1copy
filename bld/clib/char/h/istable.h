a2396e3c76a28cf0d07f747b7c2e75b9a35ccf0c
Jiri Malak <N/A>
2004-04-11 14:00:27 +0100

    ChangeLog: Fix CLIB character type check routines for 16-bit.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 26236]

--- bld/clib/char/h/istable.h
+++ bld/clib/char/h/istable.h
@@ -37,8 +37,19 @@ extern int IsWhat( int );
 #pragma aux IsWhat = \
         "and eax,0xff" \
         "mov al,_IsTable+0x1[eax]" \
-        parm [eax]
-#elif defined(__I86__) && !defined(__HUGE__)
+        parm loadds [eax]
+#elif defined(M_I86HM)
+extern int IsWhat( int );
+#pragma aux IsWhat = \
+        "push bx" \
+        "mov bx,seg _IsTable" \
+        "mov ds,bx" \
+        "and ax,0xff" \
+        "mov bx,ax" \
+        "mov al,_IsTable+0x1[bx]" \
+        "pop bx" \
+        parm [ax] modify [ds]
+#elif defined(__I86__)
 extern int IsWhat( int );
 #pragma aux IsWhat = \
         "push bx" \
@@ -46,7 +57,7 @@ extern int IsWhat( int );
         "mov bx,ax" \
         "mov al,_IsTable+0x1[bx]" \
         "pop bx" \
-        parm [ax]
+        parm loadds [ax]
 #else
 static int IsWhat( int c )
 {
