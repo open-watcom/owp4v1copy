fa99cbeb828d3ffb2e0d834a1c9e0f8bf8cc81ec
Michal Necasek <N/A>
2004-04-14 02:58:38 +0100

    Reverted Charles's change for following reasons (in increasing order of importance): - Code formatting did not conform to guidelines - Introduced platform specific code where none is IMHO needed - Completely broke OS/2 and Linux versions of wmake
    [git-p4: depot-paths = "//depot/openwatcom/": change = 26261]

--- bld/wmake/c/mautoomf.c
+++ bld/wmake/c/mautoomf.c
@@ -29,13 +29,6 @@
 *
 ****************************************************************************/
 
-#ifdef __NT__
-// ugly hack; mlex.h defines TOKEN_TYPE too
-#define TOKEN_TYPE WINTOKEN_TYPE
-#include <windows.h>
-#undef TOKEN_TYPE
-#endif
-
 #include "make.h"
 #include "mcache.h"
 #include "mmemory.h"
@@ -52,17 +45,7 @@
 #include "pcobj.h"
 
 typedef struct {
-#if defined(__NT__)
-    HANDLE      hFile,
-                hMap;
-#else
     int         handle;         // file handle of open obj file
-#endif
-#if defined(__386__) && !defined(__DOS__) && !defined(__WINDOWS__)
-    char        *Data,          // file buffer
-                *pData;         // current pointer
-    long        len;            // file length
-#endif
     time_t      time_stamp;     // time stamp of next dependancy comment
     char        *name;          // point to nameBuffer - name of next dependancy comment
 } omf_info;
@@ -80,60 +63,18 @@ typedef struct {
 } obj_comment;
 #pragma pack( pop );
 
-static BOOLEAN omfOpen( const char * name )
+static BOOLEAN verifyObjFile( int fh )
 /************************************/
 {
-#if defined(__NT__)
-    if( INVALID_HANDLE_VALUE==(fileHandle.hFile = CreateFile(name,GENERIC_READ,0,NULL,OPEN_EXISTING,FILE_ATTRIBUTE_NORMAL|FILE_FLAG_SEQUENTIAL_SCAN,NULL)) )
-        return FALSE;
-    fileHandle.len = GetFileSize(fileHandle.hFile,NULL);
-    if( 0 == fileHandle.len || 0xFFFFFFFF == fileHandle.len ) {
-        CloseHandle(fileHandle.hFile);
-        return FALSE;
-    }
-    if( NULL==(fileHandle.hMap = CreateFileMapping(fileHandle.hFile,NULL,PAGE_READONLY,0,0,NULL)) ) {
-        CloseHandle(fileHandle.hFile);
-        return FALSE;
-    }
-    if( NULL==(fileHandle.pData = fileHandle.Data = MapViewOfFile(fileHandle.hMap,FILE_MAP_READ,0,0,0)) ) {
-        CloseHandle(fileHandle.hMap);
-        CloseHandle(fileHandle.hFile);
-        return FALSE;
-    }
-#else
-    if(-1 == (fileHandle.handle = open( name, O_RDONLY|O_BINARY )) )
-        return FALSE;
-#if defined(__386__) && defined(__DOS__) && defined(__WINDOWS__)
-    else {
-        long len;
-
-        fileHandle.len      = filelength(fileHandle.handle);
-        fileHandle.pData    =
-        fileHandle.Data     = MallocSafe(fileHandle.len);
-        len = read(handl,fileHandle.Data,fileHandle.len);
-        close( fileHandle.handle );
-        if( fileHandle.len != len ) {
-            FreeSafe(fileHandle.Data);
-            return( FALSE );
-        }
-    }
-#endif
-#endif
-    return TRUE;
-}
-static BOOLEAN verifyObjFile(void)
-/************************************/
-{
-#if !defined(__386__) || defined(__DOS__) || defined(__WINDOWS__)
     auto struct {
         obj_record      header;
         obj_name        name;
     } theadr;
 
-    if( lseek( fileHandle.handle, 0, SEEK_SET ) < 0 ) {
+    if( lseek( fh, 0, SEEK_SET ) < 0 ) {
         return( FALSE );
     }
-    if( read( fileHandle.handle, &theadr, sizeof(theadr) ) != sizeof(theadr) ) {
+    if( read( fh, &theadr, sizeof(theadr) ) != sizeof(theadr) ) {
         return( FALSE );
     }
     if( theadr.header.command != CMD_THEADR ) {
@@ -142,44 +83,34 @@ static BOOLEAN verifyObjFile(void)
     if(( theadr.name.len + 2 ) != theadr.header.length ) {
         return( FALSE );
     }
-    if( lseek( fileHandle.handle, 0, SEEK_SET ) < 0 ) {
+    if( lseek( fh, 0, SEEK_SET ) < 0 ) {
         return( FALSE );
     }
-#else
-    auto struct obj {
-        obj_record      header;
-        obj_name        name;
-    } *ptheadr;
-
-    ptheadr = (struct obj*)fileHandle.Data;
-
-    if( ptheadr->header.command != CMD_THEADR ) {
-        return( FALSE );
-    }
-    if(( ptheadr->name.len + 2 ) != ptheadr->header.length ) {
-        return( FALSE );
-    }
-#endif
     return( TRUE );
 }
 
 STATIC handle OMFInitFile( const char *name ) {
 /*********************************************/
 
-    handle      ret_val = NULL;
+    int         handl;
+    handle      ret_val;
 
-    if( omfOpen(name) ) {
-        if( verifyObjFile() )
+    ret_val = NULL;
+    handl = open( name, O_RDONLY|O_BINARY );
+    if( handl != -1 ) {
+        fileHandle.handle = handl;
+        if( verifyObjFile( handl ) ) {
             ret_val = &fileHandle;
-        else
-            OMFFiniFile( &fileHandle );
+        } else {
+            close( handl );
+        }
     }
     return( ret_val );
 }
 
 static BOOLEAN getOMFCommentRecord( omf_info *info ) {
 /*************************************************/
-#if !defined(__386__) || defined(__DOS__) || defined(__WINDOWS__)
+
     obj_record          header;
     obj_comment         comment;
     int                 hdl;
@@ -209,45 +140,6 @@ static BOOLEAN getOMFCommentRecord( omf_info *info ) {
         info->name = &nameBuffer[ 0 ];
         return( TRUE );
     }
-#else
-    obj_record          *header;
-    obj_comment         *comment;
-    unsigned            len;
-
-    for(;;) {
-        header = (obj_record*)info->pData;
-        info->pData += sizeof(obj_record); // read
-        if (info->pData - info->Data >= info->len ) break;
-
-        if( header->command != CMD_COMENT ) {
-            // first LNAMES record means objfile has no dependancy info
-            if( header->command == CMD_LNAMES ) break;
-            info->pData += header->length; // seek
-            continue;
-        }
-        comment = (obj_comment*)info->pData;
-        info->pData += sizeof(obj_comment); // read
-        if (info->pData - info->Data >= info->len ) break;
-
-        if( comment->type != CMT_DEPENDENCY ) {
-            info->pData += header->length - sizeof( obj_comment ); // seek
-            continue;
-        }
-        // NULL dependency means end of dependency info
-        if( header->length < sizeof( obj_comment ) ) break;
-        // we have a dependency comment!
-        len = comment->name_len + 1;
-
-        memcpy(nameBuffer,info->pData,len);
-        info->pData += len; // read
-        if (info->pData - info->Data >= info->len ) break;
-
-        nameBuffer[ len - 1 ] = '\0';
-        info->time_stamp = _DOSStampToTime( comment->dos_date, comment->dos_time );
-        info->name = &nameBuffer[ 0 ];
-        return( TRUE );
-    }
-#endif
     return( FALSE );
 }
 
@@ -270,18 +162,7 @@ STATIC void OMFTransDep( dep_handle info, char **name, time_t *stamp ) {
 STATIC void OMFFiniFile( handle info ) {
 /**************************************/
 
-    omf_info    *pinfo  = (omf_info*)info;
-
-#if defined(__NT__)
-    UnmapViewOfFile(pinfo->Data);
-    CloseHandle(pinfo->hMap);
-    CloseHandle(pinfo->hFile);
-#else
-#if defined(__386__) && !defined(__DOS__) && !defined(__WINDOWS__)
-    FreeSafe( pinfo->Data );
-#endif
-    close( pinfo->handle );
-#endif
+    close( ((omf_info *)info)->handle );
 }
 
 const auto_dep_info OMFAutoDepInfo = {
