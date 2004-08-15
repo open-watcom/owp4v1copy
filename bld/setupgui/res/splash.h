07eee250e39532d79b6ee3f889bba07739d92717
Jiri Malak <N/A>
2004-08-15 18:01:01 +0100

    Reorganize setupgui resources to apply standard rules.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 27122]

---
+++ bld/setupgui/res/splash.h
@@ -0,0 +1,17 @@
+#if defined( CSETUP )
+BITMAP_SPLASH	BITMAP	"wcc.bmp"
+#elif defined( JCSETUP )
+BITMAP_SPLASH	BITMAP	"wcc.bmp"
+#elif defined( F77SETUP )
+BITMAP_SPLASH	BITMAP	"f77.bmp"
+#elif defined( JFSETUP )
+BITMAP_SPLASH	BITMAP	"f77.bmp"
+#elif defined( DBSETUP )
+BITMAP_SPLASH	BITMAP	"wsql.bmp"
+#elif defined( JDBSETUP )
+BITMAP_SPLASH	BITMAP	"wsql.bmp"
+#elif defined( SDBSETUP )
+BITMAP_SPLASH	BITMAP	"sqlserv.bmp"
+#else
+BITMAP_SPLASH	BITMAP	"generic.bmp"
+#endif
