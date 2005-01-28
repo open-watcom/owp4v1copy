76853d70624c7149b0a0994a0543987ca4aeb8d2
Jiri Malak <N/A>
2005-01-28 08:25:45 +0100

    Update C compiler tests.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 27933]

--- bld/ctest/regress/onetest.cmd
+++ bld/ctest/regress/onetest.cmd
@@ -1,6 +1,6 @@
 title C Test Stream
 setlocal
-set plustest_name=result
+set ctest_name=result
 wmake -h del_log
 if exist *.sav del *.sav
 rem
@@ -13,18 +13,12 @@ rem assume we want to add i86 if not Windows_NT
 :add_i86
 set extra_arch=i86
 :extra_done
-if exist local.cmd call local.cmd
 cd positive
 call onetest.cmd
 cd ..
-if exist alt_diag.cmd goto alt_diag
 cd diagnose
 call onetest.cmd
 cd ..
-goto did_diag
-:alt_diag
-call alt_diag.cmd
-:did_diag
 wmake -h clean
 wmake -h
 wmake -h clean
