e5e84ffbb8966ec802da8053a40ee750589b133f
Michal Necasek <N/A>
2005-02-04 00:40:34 +0100

    Batch file update for debugger internal docs.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 27962]

--- bld/wv/doc/ps.cmd
+++ bld/wv/doc/ps.cmd
@@ -1,6 +1,6 @@
 @set gmllib=%doc_root%\gml\syslib\
 @set gmlinc=.\
-@set gml_path=%doc_root%\gml\dos
+@set gml_path=%doc_root%\gml\os2
 @%gml_path%\wgml dbg (file ps.opt line out dbg
 @%gml_path%\wgml ovl (file ps.opt line out ovl
 @%gml_path%\wgml trap (file ps.opt line out trap
