c61773894e27a9b163cd6c6741f8de81238234d9
Peter C. Chapin <PChapin@vtc.vsc.edu>
2005-04-02 12:43:49 +0100

    Added skeleton for new std::string test program.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 28254]

---
+++ bld/plustest/regress/owstl/string02.cpp
@@ -0,0 +1,57 @@
+/****************************************************************************
+*
+*                            Open Watcom Project
+*
+*    Portions Copyright (c) 1983-2002 Sybase, Inc. All Rights Reserved.
+*
+*  ========================================================================
+*
+*    This file contains Original Code and/or Modifications of Original
+*    Code as defined in and that are subject to the Sybase Open Watcom
+*    Public License version 1.0 (the 'License'). You may not use this file
+*    except in compliance with the License. BY USING THIS FILE YOU AGREE TO
+*    ALL TERMS AND CONDITIONS OF THE LICENSE. A copy of the License is
+*    provided with the Original Code and Modifications, and is also
+*    available at www.sybase.com/developer/opensource.
+*
+*    The Original Code and all software distributed under the License are
+*    distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
+*    EXPRESS OR IMPLIED, AND SYBASE AND ALL CONTRIBUTORS HEREBY DISCLAIM
+*    ALL SUCH WARRANTIES, INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF
+*    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR
+*    NON-INFRINGEMENT. Please see the License for the specific language
+*    governing rights and limitations under the License.
+*
+*  ========================================================================
+*
+* Description:  Use this file as a template for creating new tests.
+*
+****************************************************************************/
+
+#include <iostream>
+
+#include "sanity.cpp"
+
+bool some_test( )
+{
+  bool rc = true;
+
+  // Exercise some facility here.
+
+  return( rc );
+}
+
+
+int main( )
+{
+  int rc = 0;
+  try {
+    if( !some_test( )    || !heap_ok( "t1" ) ) rc = 1;
+  }
+  catch( ... ) {
+    std::cout << "Unexpected exception of unexpected type.\n";
+    rc = 1;
+  }
+
+  return( rc );
+}
