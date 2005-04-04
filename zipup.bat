70336f3082d939adc3d2968e871742afe6181dfc
Kendall Bennett <N/A>
2005-04-05 00:51:31 +0100

    Minor updates to zipup batch files
    [git-p4: depot-paths = "//depot/openwatcom/": change = 28278]

--- zipup.bat
+++ zipup.bat
@@ -12,7 +12,7 @@ del /q %PREFIX%-src.zip >& NUL
 
 rem ##########################################################################
 rem ZIP all Open Watcom source files
-del /szx %ARCHIVES%\ow_devel_src
+del /szx %ARCHIVES%\ow_devel_src\
 p4 -uanonymous -Panonymous -cOPENWATCOM_DEVEL_SRC sync %P4OPT% @%LABEL%
 cd %ARCHIVES%\ow_devel_src
 zip -r ..\%PREFIX%-src.zip *
