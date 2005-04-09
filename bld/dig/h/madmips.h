57e07be5db7b561bd9194d363112f7eab4d12b97
Michal Necasek <N/A>
2005-04-09 03:02:23 +0100

    Reworked MIPS register storage for MAD.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 28318]

--- bld/dig/h/madmips.h
+++ bld/dig/h/madmips.h
@@ -45,35 +45,228 @@ typedef union {
     lreal               f;
 } mipsfloat;
 
+#if 0
+enum {
+    MR_f0,
+    MR_f1,
+    MR_f2,
+    MR_f3,
+    MR_f4,
+    MR_f5,
+    MR_f6,
+    MR_f7,
+    MR_f8,
+    MR_f9,
+    MR_f10,
+    MR_f11,
+    MR_f12,
+    MR_f13,
+    MR_f14,
+    MR_f15,
+    MR_f16,
+    MR_f17,
+    MR_f18,
+    MR_f19,
+    MR_f20,
+    MR_f21,
+    MR_f22,
+    MR_f23,
+    MR_f24,
+    MR_f25,
+    MR_f26,
+    MR_f27,
+    MR_f28,
+    MR_f29,
+    MR_f30,
+    MR_f31,
+
+    MR_r0,
+    MR_r1,
+    MR_r2,
+    MR_r3,
+    MR_r4,
+    MR_r5,
+    MR_r6,
+    MR_r7,
+    MR_r8,
+    MR_r9,
+    MR_r10,
+    MR_r11,
+    MR_r12,
+    MR_r13,
+    MR_r14,
+    MR_r15,
+    MR_r16,
+    MR_r17,
+    MR_r18,
+    MR_r19,
+    MR_r20,
+    MR_r21,
+    MR_r22,
+    MR_r23,
+    MR_r24,
+    MR_r25,
+    MR_r26,
+    MR_r27,
+    MR_r28,
+    MR_r29,
+    MR_r30,
+    MR_r31,
+
+    MR_pc,
+    MR_lo,
+    MR_hi,
+
+    MR_NUM_REGS,
+
+    MR_zero     = MR_r0,
+    MR_at       = MR_r1,
+    MR_v0       = MR_r2,
+    MR_v1       = MR_r3,
+    MR_a0       = MR_r4,
+    MR_a1       = MR_r5,
+    MR_a2       = MR_r6,
+    MR_a3       = MR_r7,
+    MR_t0       = MR_r8,
+    MR_t1       = MR_r9,
+    MR_t2       = MR_r10,
+    MR_t3       = MR_r11,
+    MR_t4       = MR_r12,
+    MR_t5       = MR_r13,
+    MR_t6       = MR_r14,
+    MR_t7       = MR_r15,
+    MR_s0       = MR_r16,
+    MR_s1       = MR_r17,
+    MR_s2       = MR_r18,
+    MR_s3       = MR_r19,
+    MR_s4       = MR_r20,
+    MR_s5       = MR_r21,
+    MR_s6       = MR_r22,
+    MR_s7       = MR_r23,
+    MR_t8       = MR_r24,
+    MR_t9       = MR_r25,
+    MR_k0       = MR_r26,
+    MR_k1       = MR_r27,
+    MR_gp       = MR_r28,
+    MR_sp       = MR_r29,
+    MR_s8       = MR_r30,
+    MR_ra       = MR_r31
+};
+#endif
+
 struct mips_mad_registers {
-    unsigned_64 r0;
-    unsigned_64 r1;
-    unsigned_64 r2;
-    unsigned_64 r3;
-    unsigned_64 r4;
-    unsigned_64 r5;
-    unsigned_64 r6;
-    unsigned_64 r7;
-    unsigned_64 r8;
-    unsigned_64 r9;
-    unsigned_64 r10;
-    unsigned_64 r11;
-    unsigned_64 r12;
-    unsigned_64 r13;
-    unsigned_64 r14;
-    unsigned_64 r15;
-    unsigned_64 r16;
-    unsigned_64 r17;
-    unsigned_64 r18;
-    unsigned_64 r19;
-    unsigned_64 r20;
-    unsigned_64 r21;
-    unsigned_64 r22;
-    unsigned_64 r23;
-    unsigned_64 r24;
-    unsigned_64 r25;
-    unsigned_64 r26;
-    unsigned_64 r27;
+    union {
+        unsigned_64     r0;
+        unsigned_64     zero;
+    };
+    union {
+        unsigned_64     r1;
+        unsigned_64     at;
+    };
+    union {
+        unsigned_64     r2;
+        unsigned_64     v0;
+    };
+    union {
+        unsigned_64     r3;
+        unsigned_64     v1;
+    };
+    union {
+        unsigned_64     r4;
+        unsigned_64     a0;
+    };
+    union {
+        unsigned_64     r5;
+        unsigned_64     a1;
+    };
+    union {
+        unsigned_64     r6;
+        unsigned_64     a2;
+    };
+    union {
+        unsigned_64     r7;
+        unsigned_64     a3;
+    };
+    union {
+        unsigned_64     r8;
+        unsigned_64     t0;
+    };
+    union {
+        unsigned_64     r9;
+        unsigned_64     t1;
+    };
+    union {
+        unsigned_64     r10;
+        unsigned_64     t2;
+    };
+    union {
+        unsigned_64     r11;
+        unsigned_64     t3;
+    };
+    union {
+        unsigned_64     r12;
+        unsigned_64     t4;
+    };
+    union {
+        unsigned_64     r13;
+        unsigned_64     t5;
+    };
+    union {
+        unsigned_64     r14;
+        unsigned_64     t6;
+    };
+    union {
+        unsigned_64     r15;
+        unsigned_64     t7;
+    };
+    union {
+        unsigned_64     r16;
+        unsigned_64     s0;
+    };
+    union {
+        unsigned_64     r17;
+        unsigned_64     s1;
+    };
+    union {
+        unsigned_64     r18;
+        unsigned_64     s2;
+    };
+    union {
+        unsigned_64     r19;
+        unsigned_64     s3;
+    };
+    union {
+        unsigned_64     r20;
+        unsigned_64     s4;
+    };
+    union {
+        unsigned_64     r21;
+        unsigned_64     s5;
+    };
+    union {
+        unsigned_64     r22;
+        unsigned_64     s6;
+    };
+    union {
+        unsigned_64     r23;
+        unsigned_64     s7;
+    };
+    union {
+        unsigned_64     r24;
+        unsigned_64     t8;
+    };
+    union {
+        unsigned_64     r25;
+        unsigned_64     t9;
+    };
+    union {
+        unsigned_64     r26;
+        unsigned_64     k0;
+    };
+    union {
+        unsigned_64     r27;
+        unsigned_64     k1;
+    };
     union {
         unsigned_64     r28;
         unsigned_64     gp;
@@ -82,7 +275,10 @@ struct mips_mad_registers {
         unsigned_64     r29;
         unsigned_64     sp;
     };
-    unsigned_64 r30;
+    union {
+        unsigned_64     r30;
+        unsigned_64     s8;
+    };
     union {
         unsigned_64     r31;
         unsigned_64     ra;
