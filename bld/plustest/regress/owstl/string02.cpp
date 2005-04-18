1227de1403e3851adecd211dd48072a3e2cf226e
Peter C. Chapin <PChapin@vtc.vsc.edu>
2005-04-18 02:52:08 +0100

    Additional test instantiations of std::string.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 28421]

--- bld/plustest/regress/owstl/string02.cpp
+++ bld/plustest/regress/owstl/string02.cpp
@@ -26,10 +26,13 @@
 *
 * Description:  This file contains "exotic" instantiation tests for the
 *               basic_string template. Most users won't do stuff like
-*               this, but it's supposed to work anyway.
+*               this, but it's supposed to work regardless. These tests
+*               are for the users who are doing these things.
 *
 ****************************************************************************/
 
+#include <string.h>
+#include <cctype>
 #include <iostream>
 #include <limits>
 #include <string>
@@ -38,7 +41,9 @@
 
 namespace std {
 
+  // ****************************************************************
   // Character traits specialization for type double as "characters."
+  // ****************************************************************
   template< >
   struct char_traits< double > {
     typedef double  char_type;
@@ -123,18 +128,144 @@ namespace std {
   };
 
 } // End of namespace std.
-
 typedef std::basic_string< double > dstring;
+
+// ****************************************************************
+// Character traits specialization for case insensitive comparisons.
+// ****************************************************************
+struct itraits : std::char_traits< char > {
+
+  static bool eq( const char_type &c1, const char_type &c2 )
+    { return( std::toupper( c1 ) == std::toupper( c2 ) ); }
+
+  static bool lt( const char_type &c1, const char_type &c2 )
+    { return( std::toupper( c1 ) < std::toupper( c2 ) ); }
+
+  static int compare( const char_type *s1, const char_type *s2, size_t n )
+    { return( memicmp( s1, s2, n ) ); }
+
+  // Too bad there's no memichr.
+  static const char_type *find( const char_type *s, size_t n, const char_type &a )
+  {
+    const char_type *result = 0;
+    for( size_t i = 0; i < n; ++i ) {
+      if( std::toupper( *s ) == std::toupper( a ) ) {
+        result = s;
+        break;
+      }
+      ++s;
+    }
+    return( result );
+  }
+
+  static bool eq_int_type( const int_type &c1, const int_type &c2 )
+    { return( std::toupper( c1 ) == std::toupper( c2 ) ); }
+
+};
+typedef std::basic_string< char, itraits > istring;
+
+
+// ***************************************************************
+// Character traits specialization for a structured character type.
+// ****************************************************************
+
+// std::basic_string can only be instantiated with a POD type.
+struct token {
+  int  symbol;          // Let's use this as a key field for comparisions.
+  char short_name[12];  // The other fields are arbitrary.
+  bool processed;
+
+  token() { symbol = -1; }  // This defines "end-of-string".
+  token(int s, char *p, bool f) : symbol( s ), processed( f )
+    { std::strcpy( short_name, p ); }
+};
+
+struct token_traits {
+  typedef token char_type;
+  typedef token int_type;
+
+  static void assign( char_type &c1, const char_type &c2 )
+    { c1 = c2; }
+
+  static bool eq( const char_type &c1, const char_type &c2 )
+    { return( c1.symbol == c2.symbol ); }
+
+  static bool lt( const char_type &c1, const char_type &c2 )
+  { return( c1.symbol < c2.symbol ); }
+
+  static int compare( const char_type *s1, const char_type *s2, size_t n )
+  {
+    for( size_t i = 0; i < n; ++i ) {
+      if( s1->symbol < s2->symbol ) return( -1 );
+      if( s1->symbol > s2->symbol ) return(  1 );
+      ++s1; ++s2;
+    }
+    return( 0 );
+  }
+
+  static size_t length( const char_type *s )
+  {
+    size_t count = 0;
+    while( s->symbol != -1 ) { ++count; ++s; }
+    return( count );
+  }
+
+  static const char_type *find( const char_type *s, size_t n, const char_type &a )
+  {
+    const char_type *result = 0;
+    for( size_t i = 0; i < n; ++i ) {
+      if( s->symbol == a.symbol ) {
+        result = s;
+        break;
+      }
+      ++s;
+    }
+    return( result );
+  }
+
+  static char_type *move( char_type *s1, const char_type *s2, size_t n )
+  {
+    return( static_cast< char_type * >
+            ( memmove(s1, s2, n * sizeof( char_type ) ) ) );
+  }
+
+  static char_type *copy( char_type *s1, const char_type *s2, size_t n )
+  {
+    return( static_cast< char_type * >
+            ( memcpy(s1, s2, n * sizeof( char_type ) ) ) );
+  }
+
+  static char_type *assign( char_type *s, size_t n, char_type a )
+  {
+    char_type *p = s;
+    for( size_t i = 0; i < n; ++i ) {
+      *p = a;
+      ++p;
+    }
+    return( s );
+  }
+
+  static char_type to_char_type( const int_type &c )
+  { return( static_cast< char_type >(c) ); }
+
+  static int_type to_int_type( const char_type &c )
+  { return( static_cast< int_type >(c) ); }
+
+  static bool eq_int_type( const int_type &c1, const int_type &c2 )
+  { return( c1.symbol == c2.symbol ); }
+};
+typedef std::basic_string< token, token_traits > tstring;
+
+
+// The following tests are designed to exercise the various special-
+// izations to see if the traits type is being used properly. These test
+// provide some confidence that "unusual" string types can be generated
+// from std::basic_string as allowed for by the standard. These tests
+// are not particularly exhaustive, however.
 
 bool wstring_test( )
 {
   bool rc = true;
-
-  // This test is designed to exercise wstring where the traits template
-  // is used. The idea is to make sure the traits specialization is
-  // reasonable and to spot check basic_string functionality for
-  // wchar_t. This test is not intended to be an exhaustive functional
-  // test for wstring.
 
   std::wstring s1( 10, L'x' );
   std::wstring s2( s1 );
@@ -224,6 +355,111 @@ bool dstring_test( )
   return( rc );
 }
 
+bool istring_test( )
+{
+  bool rc = true;
+
+  istring s1( 10, 'x' );
+  istring s2( s1 );
+
+  if( s1.size( ) != 10 || INSANE( s1 ) ) {
+    std::cout << "istring FAIL 0001\n"; rc = false;
+  }
+  if( s2.size( ) != 10 || s1 != s2 || INSANE( s2 ) ) {
+    std::cout << "istring FAIL 0002\n"; rc = false;
+  }
+
+  s2 = "This is a string";
+  if( s2.size( ) != 16 ||
+      s2 != "THIS IS A STRING" || INSANE( s2 ) ) {
+    std::cout << "istring FAIL 0003\n"; rc = false;
+  }
+
+  s2 = 'Z';
+  if( s2.size( ) != 1 || s2 != "z" || INSANE( s2 ) ) {
+    std::cout << "istring FAIL 0004\n"; rc = false;
+  }
+
+  istring s3( 3, 'x' ); s3[2] = 'z';
+  istring s4( 1, 'y' );
+  s3.insert( 1, s4 );
+  if( s3.size( ) != 4 || s3 != "XYxz" || INSANE( s3 ) ) {
+    std::cout << "istring FAIL 0005\n"; rc = false;
+  }
+
+  istring s5( "Hello, World" );
+  istring s6( "ELL" );
+  if( s5.find( s6 ) != 1 ) {
+    std::cout << "istring FAIL 0006\n"; rc = false;
+  }
+
+  return( rc );
+}
+
+
+bool tstring_test( )
+{
+  bool rc = true;
+
+  token raw_string[] = {
+    token( 1, "1",  true ), token( 2, "2", false ), token( 3, "3",  true ),
+    token( 4, "4", false ), token( 5, "5",  true ), token( 6, "6", false ),
+    token( 7, "7",  true ), token( 8, "8", false ), token( 9, "9",  true ),
+    token(10,"10", false ), token(11,"11",  true ), token(12,"12", false ),
+    token( )
+  };
+  token raw_value[] = { token( 2, "****", false ), token( ) };
+
+  token value_1( 1, "v1", true );
+  token value_2( 2, "v2", true );
+
+  tstring s1( 10, value_1 );
+  tstring s2( s1 );
+
+  if( s1.size( ) != 10 || INSANE( s1 ) ) {
+    std::cout << "tstring FAIL 0001\n"; rc = false;
+  }
+  if( s2.size( ) != 10 || s1 != s2 || INSANE( s2 ) ) {
+    std::cout << "tstring FAIL 0002\n"; rc = false;
+  }
+
+  s2 = raw_string;
+  if( s2.size( ) != 12 ||
+      s2 != raw_string || INSANE( s2 ) ) {
+    std::cout << "tstring FAIL 0003\n"; rc = false;
+  }
+
+  s2 = value_2;
+  if( s2.size( ) != 1 || s2 != raw_value || INSANE( s2 ) ) {
+    std::cout << "tstring FAIL 0004\n"; rc = false;
+  }
+
+  tstring s3( 3, raw_string[0] ); s3[2] = value_2;
+  tstring s4( 1, raw_string[2] );
+  token result[] = { token(1, ".", true), token(3, ".", true),
+                     token(1, ".", true), token(2, ".", true), token( ) };
+
+  s3.insert( 1, s4 );
+  if( s3.size( ) != 4 || s3 != result || INSANE( s3 ) ) {
+    std::cout << "tstring FAIL 0005\n"; rc = false;
+  }
+
+  token haystack[] = { token(1, "-",  true), token(2, "+", false),
+                       token(3, "*",  true), token(4, "/",  true),
+                       token(5, "%", false), token(6, "#",  true),
+                       token( ) };
+  token needle[] = { token(2, "A",  true), token(3, "B", false),
+                     token(4, "C", false), token( ) };
+
+  tstring s5( haystack );
+  tstring s6( needle );
+  if( s5.find( s6 ) != 1 ) {
+    std::cout << "tstring FAIL 0006\n"; rc = false;
+  }
+
+  return( rc );
+}
+
 
 int main( )
 {
@@ -233,6 +469,8 @@ int main( )
   try {
     if( !wstring_test( )    || !heap_ok( "t01" ) ) rc = 1;
     if( !dstring_test( )    || !heap_ok( "t02" ) ) rc = 1;
+    if( !istring_test( )    || !heap_ok( "t03" ) ) rc = 1;
+    if( !tstring_test( )    || !heap_ok( "t04" ) ) rc = 1;
   }
   catch( ... ) {
     std::cout << "Unexpected exception of unexpected type.\n";
