8a3b3ed55cdcc40258836ac29e4b2b0eac0b0244
WalterB <a@b>
2005-05-02 14:27:45 +0100

    Source prettifed - generated comments use all character glyphs or names.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 28612]

--- bld/wmake/c/cretype.c
+++ bld/wmake/c/cretype.c
@@ -32,67 +32,77 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-    /* foreign language support */
+    // foreign language support
 #define islang(__c)     ( ( (__c) >= 0x80 && (__c) <= 0xa7 ) || \
                         ( (__c) >= 0xe0 && (__c) <= 0xee ) )
 
-    /* macro identifiers */
+    // macro identifiers
 #define ismacc(__c)     ( isalnum(__c) || (__c) == '_' || \
                         islang(__c) || (__c) == '%' )
 
-    /* extension characters */
+    // extension characters
 #define isextc(__c)     ( isalnum(__c) || (__c) == '_' || islang(__c) || \
-                        (__c) == '-' || (__c) == '*' || (__c) == '?' || \
-                        (__c) == '&' || (__c) == '$' || (__c) == '!' || \
-                        (__c) == '#' || (__c) == '%' || (__c) == '\'' || \
-                        (__c) == '(' || (__c) == ')' || (__c) == '^' || \
-                        (__c) == '`' || (__c) == '{' || (__c) == '}' || \
-                        (__c) == '~' || (__c) == '@' \
-                        )
+                        strchr( "-*?&$!#%\\()^`{}~@", __c ) != NULL )
 
-    /* directory separator */
+    // directory separator
 #define isdirc(__c2)    ( (__c2) == '/' || (__c2) == '\\' || (__c2) == ':' )
 
-    /* filename character */
+    // filename character
 #define isfilec(__c3)   ( isextc(__c3) || isdirc(__c3) || (__c3) == '.' )
 
-    /* not quite isspace - renamed to make sure you realize difference */
+    // not quite isspace - renamed to make sure you realize difference
 #define isws(__c)       ( (__c) == ' ' || (__c) == '\t' )
 
 
 #define isprt(__c)      ( (__c) >= 32 && (__c) <= 255 )
 
-    /* is an illegal character in a file */
+    // is an illegal character in a file
 #define isbarf(__c)     ( !isprt(__c) && (__c) != '\t' && (__c) != '\a' && \
                             (__c) != '\f' && (__c) != '\n' )
 
 
-    /* this macro is used to make coding easier below */
-#define BAR( stuff )    {                   \
-    if( noneyet )   printf( #stuff );       \
-    else            printf( " | " #stuff ); \
-    noneyet = 0;                            \
-}
+    // this macro is used to make coding easier below
+#define BAR( stuff )    printf( (something_output++) ? " | " #stuff : #stuff )
 
 
 void main( void )
 /***************/
 {
-    int     i;
-    int     noneyet;
+    int                 i;
+    int                 something_output;
+    static char const   *names[] = {              // four byte names.
+        "NUL ",  "SOH ",  "STX ",  "ETX ",  "EOT ",  "ENQ ",  "ACK ",  "'\\a'",
+        "'\\b'", "'\\t'", "'\\n'", "'\\v'", "'\\f'", "'\\r'", "SO  ",  "SI  ",
+        "DLE ",  "DC1 ",  "DC2 ",  "DC3 ",  "DC4 ",  "NAK ",  "SYN ",  "ETB ",
+        "CAN ",  "EM  ",  "SUB ",  "ESC ",  "FS  ",  "GS  ",  "RS  ",  "US  "
+    };
 
     /*printf( "extern UINT8 IsArray[] = {\n" );*/
-    printf( "/*STRM_MAGIC*/  0,\n" );
-    printf( "/*STRM_END  */  0" );      /* note: no ",\n" !! */
+    printf( "/*   STRM_MAGIC  */  0,\n" );
+    printf( "/*   STRM_END    */  0" );         // no ",\n" - supplied below
 
     for( i = 0; i <= 255; i++ ) {
-        noneyet = 1;
+        something_output = 0;
 
+        printf( ",\n/*   0x%02x ", i );         // Write character value
         if( isprint( i ) ) {
-            printf( ",\n/*   '%c'    */  ", i );
+            printf( "'%c'    */  ", i );        // Character known printable
+        } else if( i < 32 ) {
+            printf( "%s   */  ", names[i] );    // Control character names
+        } else if( i == 127 ) {
+            printf( "%s   */  ", "DEL " );      // Control character name
+	}
+#ifndef __UNIX__
+	else if( i >= 128 && i <= 160 ) {
+            printf( "'%c'    */  ", i );        // !isprint but graphic glyphs
+        }
+#endif
+	else if( i >= 161 && i < 255 ) {
+            printf( "'%c'    */  ", i );        // !isprint but graphic glyphs
         } else {
-            printf( ",\n/*   0x%02x   */  ", i );
+            printf( "%s", "       */  " );      // Nothing worth saying
         }
 
         if( isws( i ) ) {
@@ -120,7 +130,7 @@ void main( void )
             BAR( IS_BARF );
         }
 
-        if( noneyet ) {
+        if( !something_output ) {
             printf( "0" );
         }
     }
