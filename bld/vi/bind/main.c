ac9d5cc281800ee1e220b6c875f6c14219ffacac
WalterB <a@b>
2005-05-02 07:51:28 +0100

    Allow Linux program to handle DOS line-ended input files
    [git-p4: depot-paths = "//depot/openwatcom/": change = 28609]

--- bld/vi/bind/main.c
+++ bld/vi/bind/main.c
@@ -232,7 +232,7 @@ static void GetFromEnv( char *what, char *path )
  */
 FILE *GetFromEnvAndOpen( char *inpath )
 {
-char tmppath[256];
+    char tmppath[256];
 
     GetFromEnv( inpath, tmppath );
     if( tmppath[0] != 0 ) {
@@ -395,11 +395,13 @@ int main( int argc, char *argv[] )
                 continue;
             }
             i = strlen( buff3 );
+            // Strip off trailing '\n' - and possible '\r' from DOS file.
+            while( i > 0 && isspace( buff3[--i] ) ) {
+                buff3[i] = 0;
+            }
             if( i == 0 ) {
                 continue;
             }
-            i--;
-            buff3[i] = 0;
             dats[ FileCount ] = MyAlloc( i+1 );
             strcpy( dats[ FileCount], buff3 );
             FileCount++;
