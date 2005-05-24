501e7bb7acb9d005f50769d34780b2c18e310d10
Michal Necasek <N/A>
2005-05-24 19:37:24 +0100

    Forgot the file header, duh...
    [git-p4: depot-paths = "//depot/openwatcom/": change = 28758]

--- bld/wl/exe2bin/exe2bin.c
+++ bld/wl/exe2bin/exe2bin.c
@@ -1,8 +1,37 @@
-// exe2bin.c
+/****************************************************************************
+*
+*                            Open Watcom Project
+*
+*    Portions Copyright (c) 1983-2002 Sybase, Inc. All Rights Reserved.
+*
+*  ========================================================================
+*
+*    This file contains Original Code and/or Modifications of Original
+*    Code as defined in and that are subject to the Sybase Open Watcom
+*    Public License version 1.0 (the 'License'). You may not use this file
+*    except in compliance with the License. BY USING THIS FILE YOU AGREE TO
+*    ALL TERMS AND CONDITIONS OF THE LICENSE. A copy of the License is
+*    provided with the Original Code and Modifications, and is also
+*    available at www.sybase.com/developer/opensource.
+*
+*    The Original Code and all software distributed under the License are
+*    distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
+*    EXPRESS OR IMPLIED, AND SYBASE AND ALL CONTRIBUTORS HEREBY DISCLAIM
+*    ALL SUCH WARRANTIES, INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF
+*    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR
+*    NON-INFRINGEMENT. Please see the License for the specific language
+*    governing rights and limitations under the License.
+*
+*  ========================================================================
+*
+* Description:  DOS EXE to binary conversion utility.
+*
+****************************************************************************/
+
 
 // this small utility just strips off the header of a dos-type exe-file and
 // performs any necessary relocations. if there are embedded relocations an
-// additional command-line argument "-l=seg" is mandatory indicating the
+// additional command-line argument "-l=seg" is mandatory, indicating the
 // address the image is supposed to be loaded at.
 
 #include <stdio.h>
