a735bec7f6fae38220904cc0d15f498b98e2a2bb
Jiri Malak <N/A>
2005-06-04 11:19:02 +0100

    Free allocated pointer to DIR structure if open does not succeed.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 28835]

--- bld/clib/posix/c/opendir.c
+++ bld/clib/posix/c/opendir.c
@@ -39,8 +39,10 @@ _WCRTLINK DIR *opendir( const char *__dirname )
     DIR *dir = malloc( sizeof( *dir ) );
 
     dir->fd = open( __dirname, O_RDONLY | O_NONBLOCK | O_DIRECTORY );
-    if (dir->fd == -1)
+    if( dir->fd == -1 ) {
+        free( dir );
         return( NULL );
+    }
     dir->bufofs = 0;
 
     return( dir );
