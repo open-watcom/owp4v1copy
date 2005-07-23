57964b74314fae9937e289d81cc48f78feb78f94
Michal Necasek <N/A>
2005-07-23 21:00:01 +0100

    Fixed formatting, changed destination directory.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 29145]

--- bld/setupgui/lang.ctl
+++ bld/setupgui/lang.ctl
@@ -14,39 +14,37 @@ cdsay .
 
 [ BLOCK <1> rel2 cprel2 ]
 #========================
+    <CPCMD> <PROJDIR>/mkdisk/mkexezip.exe <OWBINDIR>/
+
 # Note: this is at the moment only set up for one platform at the time. The <distroot>/wc directory
 # should be split into subdirs per platform, like the mkdisk dir. This is why the "" is not present
 # in the IFDEF lines. Later. 20040201 Mat Nieuwenhoven
+
   [ IFDEF (os_os2) <2*> ]
     <CPCMD> <PROJDIR>/os2386.gui/csetup.exe <distroot>/ow/os2386.gui/csetup/install.exe
     <CPCMD> <PROJDIR>/os2386.gui/virgin.sym <distroot>/ow/os2386.gui/csetup/install.sym
     <CPCMD> <PROJDIR>/os2386.gui/f77setup.exe <distroot>/os/os2386.gui/f77setup/install.exe
     <CPCMD> <PROJDIR>/os2386.gui/virgin.sym <distroot>/os/os2386.gui/f77setup/install.sym
-    <CPCMD> <PROJDIR>/mkdisk/*.exe <distroot>/supp/
   [ IFDEF (os_nt) <2*> ]
     <CPCMD> <PROJDIR>/nt386/csetup.exe <distroot>/ow/nt386/csetup/setup32.exe
     <CPCMD> <PROJDIR>/nt386/virgin.sym <distroot>/ow/nt386/csetup/setup32.sym
     <CPCMD> <PROJDIR>/nt386/f77setup.exe <distroot>/ow/nt386/f77setup/setup32.exe
     <CPCMD> <PROJDIR>/nt386/virgin.sym <distroot>/ow/nt386/f77setup/setup32.sym
-    <CPCMD> <PROJDIR>/mkdisk/*.exe <distroot>/supp/
   [ IFDEF (os_dos) <2*> ]
     <CPCMD> <PROJDIR>/dos386/setup.exe <distroot>/ow/dos386/csetup/dossetup.exe
     <CPCMD> <PROJDIR>/dos386/setup.sym <distroot>/ow/dos386/csetup/dossetup.sym
     <CPCMD> <PROJDIR>/dos386/setup.exe <distroot>/ow/dos386/f77setup/dossetup.exe
     <CPCMD> <PROJDIR>/dos386/setup.sym <distroot>/ow/dos386/f77setup/dossetup.sym
-    <CPCMD> <PROJDIR>/mkdisk/*.exe <distroot>/supp/
   [ IFDEF (os_win) <2*> ]
     <CPCMD> <PROJDIR>/wini86/csetup.exe <distroot>/ow/wini86/csetup/setup.exe
     <CPCMD> <PROJDIR>/wini86/virgin.sym <distroot>/ow/wini86/csetup/setup.sym
     <CPCMD> <PROJDIR>/wini86/f77setup.exe <distroot>/ow/wini86/f77setup/setup.exe
     <CPCMD> <PROJDIR>/wini86/virgin.sym <distroot>/ow/wini86/f77setup/setup.sym
-    <CPCMD> <PROJDIR>/mkdisk/*.exe <distroot>/supp/
   [ IFDEF (os_linux) <2*> ]
     <CPCMD> <PROJDIR>/linux386/setup.exe <distroot>/ow/linux386/csetup/setupl
     <CPCMD> <PROJDIR>/linux386/setup.sym <distroot>/ow/linux386/csetup/setupl.sym
     <CPCMD> <PROJDIR>/linux386/setup.exe <distroot>/ow/linux386/f77setup/setupl
     <CPCMD> <PROJDIR>/linux386/setup.sym <distroot>/ow/linux386/f77setup/setupl.sym
-    <CPCMD> <PROJDIR>/mkdisk/*.exe <distroot>/supp/
 
 
 [ BLOCK <1> clean ]
--- bld/setupgui/mkdisk/mkexezip.c
+++ bld/setupgui/mkdisk/mkexezip.c
@@ -24,7 +24,7 @@
 *
 *  ========================================================================
 *
-* Description:  Utility to create installer with zip-file at end of it.
+* Description:  Utility to create a self-extracting ZIP archive.
 *
 ****************************************************************************/
 
@@ -38,256 +38,239 @@
 
 #define BUFFER_SIZE 16384
 
-#pragma pack(push);
-#pragma pack(1);
+#pragma pack(1)
+
 typedef struct {
-    char signature[4];
-    uint16_t disk_number; // not supported by libzip
-    uint16_t disk_having_cd; // not supported by libzip
-    uint16_t num_entries_on_disk;  // not supported by libzip
-    uint16_t total_num_entries;
-
-    uint32_t cd_size;
-    uint32_t cd_offset;
-    uint16_t comment_length;
-
+    char        signature[4];
+    uint16_t    disk_number;            // not supported by libzip
+    uint16_t    disk_having_cd;         // not supported by libzip
+    uint16_t    num_entries_on_disk;    // not supported by libzip
+    uint16_t    total_num_entries;
+    uint32_t    cd_size;
+    uint32_t    cd_offset;
+    uint16_t    comment_length;
 } ZIP_END_OF_CENTRAL_DIRECTORY;
 
 typedef struct {
-    char signature[4];
-    uint16_t version_made_by;
-    uint16_t version_needed;
-    uint16_t flags;
-    uint16_t method;
-    uint16_t mod_time;
-    uint16_t mod_date;
-    uint32_t crc32;
-    uint32_t compressed_size;
-    uint32_t uncompressed_size;
-
-    uint16_t file_name_length;
-    uint16_t extra_field_length;
-    uint16_t file_comment_length;
-
-    uint16_t disk; // not supported by libzip
-
-    uint16_t int_attrib;
-    uint32_t ext_attrib;
-    uint32_t offset;
-
+    char        signature[4];
+    uint16_t    version_made_by;
+    uint16_t    version_needed;
+    uint16_t    flags;
+    uint16_t    method;
+    uint16_t    mod_time;
+    uint16_t    mod_date;
+    uint32_t    crc32;
+    uint32_t    compressed_size;
+    uint32_t    uncompressed_size;
+    uint16_t    file_name_length;
+    uint16_t    extra_field_length;
+    uint16_t    file_comment_length;
+    uint16_t    disk;                   // not supported by libzip
+    uint16_t    int_attrib;
+    uint32_t    ext_attrib;
+    uint32_t    offset;
 } ZIP_CENTRAL_DIRECTORY_FILE_HEADER;
 
-#pragma pack(pop);
+#pragma pack()
 
 static char usage[] = "%s <target exe> <source zip>\n";
 
-static int find(char *buffer, uint32_t buffer_len, char *match, uint32_t match_len);
-static int fix_cd(FILE *f, char *buffer, uint32_t buffer_len, uint32_t offset);
+static int find( char *buffer, uint32_t buffer_len, char *match, uint32_t match_len )
+{
+    char    *ptr = buffer;
+    int     i;
 
-int main(int argc, char *argv[]) {
-    FILE *ftarget;
-    FILE *fsource;
-    char *buffer = NULL;
-    char *prg;
-    char *target;
-    char *source;
-
-    uint32_t offset;
-    uint32_t length;
-    size_t n;
-    int pos;
-
-    prg = argv[0];
-
-    if (argc < 3) {
-	fprintf(stderr, usage, prg);
-	exit(2);
-    }
-
-    target = argv[1];
-    source = argv[2];
-
-    ftarget = fopen(target, "r+b");
-    if (ftarget == NULL) {
-        fprintf(stderr, "can't open %s for writing\n", target);
-        return 1;
-    }
-
-    fsource = fopen(source, "rb");
-    if (fsource == NULL) {
-        fprintf(stderr, "can't open %s for reading\n", source);
-        fclose(ftarget);
-        return 1;
-    }
-
-    // seek at end of target and save it's original size
-    fseek(ftarget, 0, SEEK_END);
-    offset = ftell(ftarget);
-
-    buffer = (char *)malloc(BUFFER_SIZE);
-    if (buffer == NULL) {
-        fprintf(stderr, "not enough memory for buffer\n");
-        fclose(ftarget);
-        fclose(fsource);
-        return 1;
-    }
-
-    // --- copy zip ---
-    while((n = fread(buffer, 1, BUFFER_SIZE, fsource)) != 0) {
-        if (fwrite(buffer, 1, n, ftarget) != n) {
-            fprintf(stderr, "can't write to %s\n", target);
-            fclose(ftarget);
-            fclose(fsource);
-            free(buffer);
-            return 1;
-        }
-    }
-
-    // source zip is now copied
-    fclose(fsource);
-
-    // save length of target for later usage
-    length = ftell(ftarget);
-
-    // --- fixup offsets ---
-
-    // find end of central directory
-    fseek(ftarget, -((length < BUFFER_SIZE) ? length : BUFFER_SIZE), SEEK_END);
-    n = fread(buffer, 1, BUFFER_SIZE, ftarget);
-
-    if (n == 0) {
-        fprintf(stderr, "odd, failed to read data from end of file %s\n", target);
-        fclose(ftarget);
-        free(buffer);
-        return 1;
-    }
-
-    pos = 0;
-    while(pos != -1)
-    {
-        pos = find(buffer+pos, n-pos, EOCD_MAGIC, strlen(EOCD_MAGIC));
-        if (pos == -1) {
-            fprintf(stderr, "error, no zip magic found from %s\n", target);
-            fclose(ftarget);
-            free(buffer);
-            return 1;
-        }
-
-        if (fix_cd(ftarget, buffer + pos, n - pos, offset))
-        {
-            // fixup also eocd
-            ZIP_END_OF_CENTRAL_DIRECTORY eocd;
-            uint32_t off;
-
-            off = -((length < BUFFER_SIZE) ? length : BUFFER_SIZE);
-            off+= pos;
-
-            fseek(ftarget, off, SEEK_END);
-
-            if (fread(&eocd, sizeof(ZIP_END_OF_CENTRAL_DIRECTORY), 1, ftarget) != 1)
-            {
-                fprintf(stderr, "file read error: %s\n", target);
-                fclose(ftarget);
-                free(buffer);
-                return 1;
-            }
-
-            if (find((char *)&eocd, 4, EOCD_MAGIC, 4) != 0)
-            {
-                fprintf(stderr, "file read error while checking eocd magic: %s\n", target);
-                fclose(ftarget);
-                free(buffer);
-                return 1;
-            }
-
-            eocd.cd_offset += offset;
-
-            fseek(ftarget, off, SEEK_END);
-            fwrite(&eocd, sizeof(ZIP_END_OF_CENTRAL_DIRECTORY), 1, ftarget);
-
-            break;
-        } else
-        {
-            pos++;
-        }
-    }
-
-    // --- cleanup  ---
-    fclose(ftarget);
-    free(buffer);
-    return 0;
-}
-
-static int find(char *buffer, uint32_t buffer_len, char *match, uint32_t match_len) {
-    char *ptr = buffer;
-    int i;
-
-    while(ptr != NULL)
-    {
+    while( ptr != NULL ) {
         // find startup character
-        ptr = memchr(ptr, match[0], buffer_len - (ptr - buffer));
-        if (ptr == NULL) break;
+        ptr = memchr( ptr, match[0], buffer_len - (ptr - buffer) );
+        if( ptr == NULL )
+            break;
 
         // make sure there is room for our match
-        if (((ptr - buffer) + match_len) > buffer_len) {
+        if( ((ptr - buffer) + match_len) > buffer_len )
             break;
-        }
 
         // check for match
-        for (i = 1; i < match_len; i++) {
-            if (ptr[i] != match[i])
+        for( i = 1; i < match_len; i++ ) {
+            if( ptr[i] != match[i] )
                 break;
         }
 
-        // if match found, return it's position in buffer
-        if (i == match_len) {
-            return ptr - buffer;
+        // if match found, return its position in buffer
+        if( i == match_len ) {
+            return( ptr - buffer );
         }
 
         // skip to next occurance
         ptr++;
     }
 
-    return -1;
+    return( -1 );
 }
 
-static int fix_cd(FILE *f, char *buffer, uint32_t buffer_len, uint32_t offset)
-{
-    ZIP_END_OF_CENTRAL_DIRECTORY *eocd;
-    ZIP_CENTRAL_DIRECTORY_FILE_HEADER fileheader;
-    uint32_t header_pos;
-    uint32_t i;
 
-    eocd = (ZIP_END_OF_CENTRAL_DIRECTORY *)buffer;
+static int fix_cd( FILE *f, void *buffer, uint32_t buffer_len, uint32_t offset )
+{
+    ZIP_END_OF_CENTRAL_DIRECTORY        *eocd;
+    ZIP_CENTRAL_DIRECTORY_FILE_HEADER   fileheader;
+    uint32_t                            header_pos;
+    uint32_t                            i;
+
+    eocd = buffer;
 
     header_pos = offset + eocd->cd_offset;
 
     // fixup all files in central directory
-    for (i = 0; i < eocd->total_num_entries; i++)
-    {
-        fseek(f, header_pos, SEEK_SET);
+    for( i = 0; i < eocd->total_num_entries; i++ ) {
+        fseek( f, header_pos, SEEK_SET );
 
-        if (fread(&fileheader, sizeof(ZIP_CENTRAL_DIRECTORY_FILE_HEADER), 1, f) != 1)
-        {
-            return 0;
+        if( fread( &fileheader, sizeof( fileheader ), 1, f ) != 1 ) {
+            return( 0 );
         }
 
-        // make sure we are at correct position in file
-        if (find((char *)&fileheader, 4, CENTRAL_MAGIC, 4) != 0)
-        {
-            return 0;
+        // make sure we are at the correct position in file
+        if( find( (char *)&fileheader, 4, CENTRAL_MAGIC, 4 ) != 0) {
+            return( 0 );
         }
 
         fileheader.offset += offset;
-        fseek(f, header_pos, SEEK_SET);
-        fwrite(&fileheader, sizeof(ZIP_CENTRAL_DIRECTORY_FILE_HEADER), 1, f);
+        fseek( f, header_pos, SEEK_SET );
+        fwrite( &fileheader, sizeof( fileheader ), 1, f );
 
         // move to next entry
-        header_pos += sizeof(ZIP_CENTRAL_DIRECTORY_FILE_HEADER);
+        header_pos += sizeof( fileheader );
         header_pos += fileheader.file_name_length;
         header_pos += fileheader.extra_field_length;
         header_pos += fileheader.file_comment_length;
     }
+    return( 1 );
+}
 
-    return 1;
+
+int main( int argc, char *argv[] )
+{
+    FILE        *ftarget;
+    FILE        *fsource;
+    char        *buffer = NULL;
+    char        *prg;
+    char        *target;
+    char        *source;
+    uint32_t    offset;
+    uint32_t    length;
+    size_t      n;
+    int         pos;
+
+    prg = argv[0];
+
+    if( argc < 3 ) {
+        fprintf( stderr, usage, prg );
+        exit( 2 );
+    }
+
+    target = argv[1];
+    source = argv[2];
+
+    ftarget = fopen( target, "r+b" );
+    if( ftarget == NULL ) {
+        fprintf( stderr, "can't open '%s' for writing\n", target );
+        return( 1 );
+    }
+
+    fsource = fopen( source, "rb" );
+    if( fsource == NULL ) {
+        fprintf( stderr, "can't open '%s' for reading\n", source );
+        fclose( ftarget );
+        return( 1 );
+    }
+
+    // seek at end of target and save it's original size
+    fseek( ftarget, 0, SEEK_END );
+    offset = ftell( ftarget );
+
+    buffer = malloc( BUFFER_SIZE );
+    if( buffer == NULL ) {
+        fprintf( stderr, "not enough memory for buffer\n" );
+        fclose( ftarget );
+        fclose( fsource );
+        return( 1 );
+    }
+
+    // --- copy zip ---
+    while( (n = fread( buffer, 1, BUFFER_SIZE, fsource )) != 0 ) {
+        if( fwrite( buffer, 1, n, ftarget ) != n ) {
+            fprintf( stderr, "can't write to '%s'\n", target );
+            fclose( ftarget );
+            fclose( fsource );
+            free( buffer );
+            return( 1 );
+        }
+    }
+
+    // source zip is now copied
+    fclose( fsource );
+
+    // save length of target for later usage
+    length = ftell( ftarget );
+
+    // --- fixup offsets ---
+
+    // find end of central directory
+    fseek( ftarget, -((length < BUFFER_SIZE) ? length : BUFFER_SIZE), SEEK_END );
+    n = fread( buffer, 1, BUFFER_SIZE, ftarget );
+
+    if( n == 0 ) {
+        fprintf( stderr, "error: failed to read data from end of file '%s'\n", target );
+        fclose( ftarget );
+        free( buffer );
+        return( 1 );
+    }
+
+    pos = 0;
+    while( pos != -1 ) {
+        pos = find( buffer + pos, n - pos, EOCD_MAGIC, strlen( EOCD_MAGIC ) );
+        if( pos == -1 ) {
+            fprintf( stderr, "error: no ZIP magic found in '%s'\n", target );
+            fclose( ftarget );
+            free( buffer );
+            return( 1 );
+        }
+
+        if( fix_cd( ftarget, buffer + pos, n - pos, offset ) ) {
+            // fixup also eocd
+            ZIP_END_OF_CENTRAL_DIRECTORY    eocd;
+            uint32_t                        off;
+
+            off  = -((length < BUFFER_SIZE) ? length : BUFFER_SIZE);
+            off += pos;
+
+            fseek( ftarget, off, SEEK_END );
+
+            if( fread( &eocd, sizeof( eocd ), 1, ftarget ) != 1 ) {
+                fprintf( stderr, "file read error: '%s'\n", target );
+                fclose( ftarget );
+                free( buffer );
+                return( 1 );
+            }
+
+            if( find( (char *)&eocd, 4, EOCD_MAGIC, 4 ) != 0 ) {
+                fprintf( stderr, "file read error while checking eocd magic: '%s'\n", target );
+                fclose( ftarget );
+                free( buffer );
+                return( 1 );
+            }
+
+            eocd.cd_offset += offset;
+
+            fseek( ftarget, off, SEEK_END );
+            fwrite( &eocd, sizeof( eocd ), 1, ftarget );
+            break;
+        } else {
+            pos++;
+        }
+    }
+
+    // --- cleanup  ---
+    fclose( ftarget );
+    free( buffer );
+    return( 0 );
 }
