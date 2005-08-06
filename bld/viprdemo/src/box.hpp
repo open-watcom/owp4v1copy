be4973fa60a522f3ed7a1a14e75262107be59cdb
Michal Necasek <N/A>
2005-08-06 03:17:31 +0100

    Added descriptions to IDE sample source files.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 29211]

--- bld/viprdemo/src/box.hpp
+++ bld/viprdemo/src/box.hpp
@@ -24,8 +24,7 @@
 *
 *  ========================================================================
 *
-* Description:  WHEN YOU FIGURE OUT WHAT THIS FILE DOES, PLEASE
-*               DESCRIBE IT HERE!
+* Description:  Low level classes for 3D objects.
 *
 ****************************************************************************/
 
