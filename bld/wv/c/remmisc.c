2e16163b7cc6b5619655897174375412edf2a2d1
Jiri Malak <N/A>
2005-08-10 18:28:40 +0100

    Add explicit initialization for var.remote.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 29258]

--- bld/wv/c/remmisc.c
+++ bld/wv/c/remmisc.c
@@ -125,6 +125,7 @@ void InitTrap( char *trap_file )
     in_mx.req = REQ_CONNECT;
     in_mx.ver.major = TRAP_MAJOR_VERSION;
     in_mx.ver.minor = TRAP_MINOR_VERSION;
+    in_mx.ver.remote = FALSE;
     in[0].ptr = &in_mx;
     in[0].len = sizeof( in_mx );
     out[0].ptr = &out_mx;
