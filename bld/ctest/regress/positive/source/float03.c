017ab88cd2a01089761fdd59742d73d83ecde806
Michal Necasek <N/A>
2005-09-20 19:45:07 +0100

    Fixed/hacked float03 test to pass on Alpha. It is not possible to achieve as much precision as x86 has with long double, although we should be able to do a little better than now.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 29439]

--- bld/ctest/regress/positive/source/float03.c
+++ bld/ctest/regress/positive/source/float03.c
@@ -1,10 +1,22 @@
 #include "fail.h"
 
+/*
+ * Open Watcom on x86 "correctly" compiles the following on x86,
+ * because it internally uses long double (80-bit) accuracy. Most
+ * if not all other x86 compilers fail this test. Open Watcom on
+ * non-x86 platforms such as Alpha or MIPS only has 64-bit doubles
+ * available, hence we need to decrease the number of significant
+ * digits.
+ * NB: It should be possible to use two more digits on non-x86,
+ * since DEC/MS compiler on Alpha can do it.
+ */
+ 
 double c[] = {
     1.2 - .2,
     .9 + .1,
     .3 * 10,
     3 / .2,
+#ifdef __X86__
     .1111111111111111111 <
     .1111111111111111112,
     .1111111111111111111 <=
@@ -17,7 +29,26 @@ double c[] = {
     .1111111111111111112,
     .1111111111111111111 !=
     .1111111111111111112,
+#else
+    .1111111111111111 <
+    .1111111111111112,
+    .1111111111111111 <=
+    .1111111111111112,
+    .1111111111111111 >
+    .1111111111111112,
+    .1111111111111111 >=
+    .1111111111111112,
+    .1111111111111111 ==
+    .1111111111111112,
+    .1111111111111111 !=
+    .1111111111111112,
+#endif
+#ifdef __WATCOMC__
+    /* This isn't standard C! */
     ( .23 , 1.25 )
+#else
+    1.25
+#endif
 };
 
 unsigned n[] = {
@@ -52,7 +83,12 @@ int main() {
     unsigned *dd;
     double *p;
 
+#ifdef __X86__
     if( (unsigned)34.99999999999999999999999999999999999999 != 34 ) fail(__LINE__);
+#else
+    /* Same as above - non-x86 platforms don't have enough accuracy */
+    if( (unsigned)34.99999999999999 != 34 ) fail(__LINE__);
+#endif
     if( ((double)2)/2/2*2*2 != 2 ) fail(__LINE__);
     nn = n;
     dd = d;
