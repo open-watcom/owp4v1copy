fb81d954d3e281c94d9c75d0a62fcbac8b9efb05
Jiri Malak <N/A>
2006-01-02 23:56:55 +0100

    Re-enable FAR CALLS optimization.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 29807]

--- bld/wl/c/wlink.c
+++ bld/wl/c/wlink.c
@@ -345,7 +345,7 @@ static void ResetMisc( void )
 /***************************/
 /* Linker support initialization. */
 {
-    LinkFlags = REDEFS_OK | CASE_FLAG;
+    LinkFlags = REDEFS_OK | CASE_FLAG | FAR_CALLS_FLAG;
     LinkState = MAKE_RELOCS;
     AbsGroups = NULL;
     DataGroup = NULL;
