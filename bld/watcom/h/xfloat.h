932b833bc33d24c0dd5ee595f34d75656b1798c1
Michal Necasek <N/A>
2006-03-03 08:35:57 +0100

    Reordered floating-point classification enum values to be sorted by magnitude (zero, subnormal, normal, nan, inf). Allows more efficient tests for (non-)finite numbers.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 30166]

--- bld/watcom/h/xfloat.h
+++ bld/watcom/h/xfloat.h
@@ -92,12 +92,13 @@ typedef struct {                // Layout of IEEE 754 single (FS)
     };
 } float_single;
 
+/* NB: The following values *must* match FP_ macros in math.h! */
 enum    ld_classification {
     __ZERO      = 0,
-    __NONZERO   = 1,
-    __NAN       = 2,
-    __INFINITY  = 3,
-    __DENORMAL  = 4
+    __DENORMAL  = 1,
+    __NONZERO   = 2,
+    __NAN       = 3,
+    __INFINITY  = 4
 };
 
 enum    ldcvt_flags {
