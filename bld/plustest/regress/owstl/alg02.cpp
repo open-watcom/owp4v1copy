1f8b8834ee9d37ba32b475abb1ec3f7f64063091
Peter C. Chapin <PChapin@vtc.vsc.edu>
2006-05-11 13:19:30 +0100

    Elaborated on the regression test for std::sort. The old test gave extremely inadequate coverage for the new algorithm.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 30555]

--- bld/plustest/regress/owstl/alg02.cpp
+++ bld/plustest/regress/owstl/alg02.cpp
@@ -30,94 +30,160 @@
 #include <algorithm>
 #include <iostream>
 
-bool heap_test( )
-{
-  bool rc = true;
-
-  // Exercise heaps here.
-
-  // Currently std::sort is implemented in terms of heaps so perhaps we
-  // can get by without testing anything here for right now (obviously
-  // this needs to be fixed at some point).
-
-  return( rc );
-}
+#include "sanity.cpp"
 
 //
-// Test cases can contain no more than this many elements.
+// Sort test cases can contain no more than this many elements.
 //
-const int MAX_SIZE = 9;
+const int MAX_SIZE = 32;
 
 //
 // The following structure holds information about a single test case.
 //
 struct test_case {
   char *title;              // Something to print when reporting results.
-  char  input[MAX_SIZE];    // The array to sort.
-  char  expected[MAX_SIZE]; // The expected result.
+  int   input[MAX_SIZE];    // The array to sort.
+  int   expected[MAX_SIZE]; // The expected result.
   int   size;               // The number of elements in input[] I care about.
 };
 
-
 //
-// The test cases.
+// The test cases. The current version of std::sort uses QuickSort but
+// falls over to InsertionSort on subsequences of length <= 10. Thus it is
+// important to use test cases that are longer than 10 to properly exercise
+// both algorithms.
+//
+// Note that the 'title' member is no longer used but it is retained for
+// documentation and possible future use.
 //
 struct test_case tests[] = {
 
-  { "An empty sequence ................",
-    { '@' },
-    { '@' }, 0 },
+  // The first few test cases are short. They only exercise the Insertion-
+  // Sort subset of std::sort's behavior.
 
-  { "One element ......................", 
-    { 'a' },
-    { 'a' }, 1 },
+  { "SHORT: An empty sequence",
+    { 0 },        // Open Watcom v1.5 doesn't accept { } as an initializer.
+    { 0 }, 0 },
 
-  { "Two elements .....................",
-    { 'b', 'a' },
-    { 'a', 'b' }, 2 },
+  { "SHORT: One element", 
+    { 1 },
+    { 1 }, 1 },
 
-  { "Three elements ...................",
-    { 'b', 'c', 'a' },
-    { 'a', 'b', 'c' }, 3 },
+  { "SHORT: Two elements",
+    { 2, 1 },
+    { 1, 2 }, 2 },
 
-  { "Random elements, size 2^n ........",
-    { 'd', 'b', 'h', 'e', 'c', 'g', 'f', 'a' },
-    { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h' }, 8 },
+  { "SHORT: Three elements",
+    { 2, 3, 1 },
+    { 1, 2, 3 }, 3 },
 
-  { "Random elements, size 2^n + 1 ....",
-    { 'a', 'h', 'c', 'b', 'd', 'i', 'e', 'g', 'f' },
-    { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i' }, 9 },
+  { "SHORT: Random elements, size 2^n",
+    { 4, 2, 8, 5, 3, 7, 6, 1 },
+    { 1, 2, 3, 4, 5, 6, 7, 8 }, 8 },
 
-  { "Random elements, size 2^n - 1 ....",
-    { 'd', 'b', 'e', 'c', 'a', 'g', 'f' },
-    { 'a', 'b', 'c', 'd', 'e', 'f', 'g' }, 7 },
+  { "SHORT: Random elements, size 2^n + 1",
+    { 1, 8, 3, 2, 4, 9, 5, 7, 6 },
+    { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 9 },
 
-  { "Already sorted ...................",
-    { 'a', 'b', 'c', 'd', 'e' },
-    { 'a', 'b', 'c', 'd', 'e' }, 5 },
+  { "SHORT: Random elements, size 2^n - 1",
+    { 4, 2, 5, 3, 1, 7, 6 },
+    { 1, 2, 3, 4, 5, 6, 7 }, 7 },
 
-  { "Already reverse sorted ...........",
-    { 'e', 'd', 'c', 'b', 'a' },
-    { 'a', 'b', 'c', 'd', 'e' }, 5 },
+  { "SHORT: Already sorted",
+    { 1, 2, 3, 4, 5 },
+    { 1, 2, 3, 4, 5 }, 5 },
 
-  { "One pair of duplicate elements ...",
-    { 'a', 'b', 'd', 'b', 'c' },
-    { 'a', 'b', 'b', 'c', 'd' }, 5 },
+  { "SHORT: Already reverse sorted",
+    { 5, 4, 3, 2, 1 },
+    { 1, 2, 3, 4, 5 }, 5 },
 
-  { "All duplicate elements ...........",
-    { 'a', 'a', 'a', 'a', 'a' },
-    { 'a', 'a', 'a', 'a', 'a' }, 5 }
+  { "SHORT: One pair of duplicate elements",
+    { 1, 2, 4, 2, 3 },
+    { 1, 2, 2, 3, 4 }, 5 },
+
+  { "SHORT: All duplicate elements",
+    { 1, 1, 1, 1, 1 },
+    { 1, 1, 1, 1, 1 }, 5 },
+
+  // The following tests are all length 11. This is the first length at
+  // which a partitioning is done.
+
+  { "LONG: Random elements; median3 in middle",
+    {  3,  4,  1,  9, 10,  5,  7, 11,  2,  6,  8 },
+    {  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11 }, 11 },
+
+  { "LONG: Random elements; median3 on left",
+    {  5,  4,  1,  9, 10,  8,  7, 11,  2,  6,  3 },
+    {  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11 }, 11 },
+
+  { "LONG: Random elements; median3 on right",
+    {  3,  4,  1,  9, 10,  8,  7, 11,  2,  6,  5 },
+    {  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11 }, 11 },
+
+  { "LONG: Already sorted",
+    {  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11 },
+    {  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11 }, 11 },
+
+  { "LONG: Already reversed sorted",
+    { 11, 10,  9,  8,  7,  6,  5,  4,  3,  2,  1 },
+    {  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11 }, 11 },
+
+  { "LONG: All duplicate elements",
+    {  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1 },
+    {  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1 }, 11 },
+
+  // The following tests are all long enough to cause multiple recursions.
+
+  { "VERY LONG: Random elements",
+    { 4, 18, 5, 1, 9, 7, 12, 20, 6, 13, 2, 3, 14, 8, 10, 15, 11, 19, 17, 16, 21 },
+    { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21 }, 21 }
 };
+const int number_cases = sizeof(tests)/sizeof(test_case);
+
+bool heap_test( )
+{
+  bool rc = true;
+
+  // Since sorting a heap gives most of the other heap related code a good
+  // workout, I will "borrow" the sort test cases to do a quick and dirty
+  // heap test. At some point it might be nice to build a more heap-
+  // specific test.
+
+  // First I must copy the test cases.
+  // I don't want to leave them all sorted for the sort_test!
+  //
+  test_case *tc = new test_case[number_cases];
+  for( int i = 0; i < number_cases; ++i ) tc[i] = tests[i];
+
+  // Now basically do the same thing as in sort_test.
+
+  // For each test...
+  for( int i = 0; i < number_cases; ++i ) {
+
+    // Do the test.
+    std::make_heap( &tc[i].input[0], &tc[i].input[tc[i].size] );
+    std::sort_heap( &tc[i].input[0], &tc[i].input[tc[i].size] );
+
+    // Did it work?
+    bool worked = true;
+    for( int j = 0; j < tc[i].size; ++j ) {
+      if ( tc[i].input[j] != tc[i].expected[j] ) worked = false;
+    }
+    if ( !worked ) FAIL;
+  }
+
+  delete [] tc;
+  return( rc );
+}
 
 bool sort_test( )
 {
   bool rc = true;
 
   // For each test...
-  for( int i = 0; i < sizeof(tests)/sizeof(struct test_case); ++i ) {
+  for( int i = 0; i < number_cases; ++i ) {
 
     // Do the test.
-    std::cout << tests[i].title << " " << std::flush;
     std::sort( &tests[i].input[0], &tests[i].input[tests[i].size] );
 
     // Did it work?
@@ -125,12 +191,7 @@ bool sort_test( )
     for( int j = 0; j < tests[i].size; ++j ) {
       if ( tests[i].input[j] != tests[i].expected[j] ) worked = false;
     }
-    if ( worked )
-      std::cout << "passed.\n";
-    else {
-      std::cout << "FAILED!\n";
-      rc = false;
-    }
+    if ( !worked ) FAIL;
   }
 
   return( rc );
@@ -140,14 +201,20 @@ bool sort_test( )
 int main( )
 {
   int rc = 0;
+  int original_count = heap_count( );
+
   try {
-    if( !heap_test( )       ) rc = 1;
-    if( !sort_test( )       ) rc = 1;
+    if( !heap_test( ) || !heap_ok( "t01" ) ) rc = 1;
+    if( !sort_test( ) || !heap_ok( "t02" ) ) rc = 1;
   }
   catch( ... ) {
     std::cout << "Unexpected exception of unexpected type.\n";
     rc = 1;
   }
 
+  if( heap_count( ) != original_count ) {
+    std::cout << "Possible memory leak!\n";
+    rc = 1;
+  }
   return( rc );
 }
