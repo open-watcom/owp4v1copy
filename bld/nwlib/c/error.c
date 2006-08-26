f7c103af72d45de105ed821a96416623fcbdd5c5
Michal Necasek <N/A>
2006-08-26 23:07:27 +0100

    Corrected res_seek prototype. There is still a mismatch with tell().
    [git-p4: depot-paths = "//depot/openwatcom/": change = 30766]

--- bld/nwlib/c/error.c
+++ bld/nwlib/c/error.c
@@ -42,7 +42,7 @@ extern  long            FileShift;
 
 
 
-static long res_seek( int handle, off_t position, int where )
+static off_t res_seek( WResFileID handle, off_t position, int where )
 /* fool the resource compiler into thinking that the resource information
  * starts at offset 0 */
 {
