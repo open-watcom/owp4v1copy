fa1eb4ee378c84b7ed7122d9f5da0cdad3b10653
Michal Necasek <N/A>
2006-09-15 05:32:09 +0100

    Updater objdump to properly dump 16-bit OMF objects.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 30848]

--- bld/orl/h/orlglobl.h
+++ bld/orl/h/orlglobl.h
@@ -215,6 +215,7 @@ typedef enum {
     ORL_SYM_CDAT_SHIFT                  = 13
 } orl_symbol_type;
 
+// relocation types - add new types at end if possible, also update objdump!
 typedef enum {
     ORL_RELOC_TYPE_NONE,        // error type
     ORL_RELOC_TYPE_ABSOLUTE,    // ref to a 32-bit absolute address
@@ -240,14 +241,6 @@ typedef enum {
     ORL_RELOC_TYPE_REL_32,      // relative ref to a 32-bit address
     ORL_RELOC_TYPE_REL_32_NOADJ,// as above, but doesn't need -4 adjustment
 
-// these special REL_32_ADJx are for x64 coff files, there are only used in ndisasm yet!
-// for information look into watcom/h/coff.h in amd64 section for more information
-    ORL_RELOC_TYPE_REL_32_ADJ1, // relative ref to a 32-bit address, need special adjustment
-    ORL_RELOC_TYPE_REL_32_ADJ2, // relative ref to a 32-bit address, need special adjustment
-    ORL_RELOC_TYPE_REL_32_ADJ3, // relative ref to a 32-bit address, need special adjustment
-    ORL_RELOC_TYPE_REL_32_ADJ4, // relative ref to a 32-bit address, need special adjustment
-    ORL_RELOC_TYPE_REL_32_ADJ5, // relative ref to a 32-bit address, need special adjustment
-    
     ORL_RELOC_TYPE_TOCREL_16,   // relative ref to 16-bit offset from TOC base.
     ORL_RELOC_TYPE_TOCREL_14,   // rel. ref to 14-bit offset from TOC base shifted 2.
     ORL_RELOC_TYPE_TOCVREL_16,  // ditto, data explicitely defined in .tocd
@@ -275,6 +268,14 @@ typedef enum {
     ORL_RELOC_TYPE_REL_HI_8,    // relative reference high byte of 16 bit offset
     ORL_RELOC_TYPE_WORD_32_SEG, // 32-bit offset and segment
     ORL_RELOC_TYPE_REL_32_SEG,  // relative reference 32 bit offset and segment
+
+    // special relications for x64 coff files, there are only used in ndisasm yet!
+    // for more information see watcom/h/coff.h in amd64 section
+    ORL_RELOC_TYPE_REL_32_ADJ1, // relative ref to a 32-bit address, need special adjustment
+    ORL_RELOC_TYPE_REL_32_ADJ2, // relative ref to a 32-bit address, need special adjustment
+    ORL_RELOC_TYPE_REL_32_ADJ3, // relative ref to a 32-bit address, need special adjustment
+    ORL_RELOC_TYPE_REL_32_ADJ4, // relative ref to a 32-bit address, need special adjustment
+    ORL_RELOC_TYPE_REL_32_ADJ5, // relative ref to a 32-bit address, need special adjustment
 
     ORL_RELOC_TYPE_MAX          // Must be last value in this enum
 } orl_reloc_type;
