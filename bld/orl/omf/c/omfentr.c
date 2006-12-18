f4e9da9334eca19362e1daf7cee60fd73e170b1a
Jiri Malak <N/A>
2006-12-18 14:51:34 +0100

    Fix ORL find section for OMF if section name is specified.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 31137]

--- bld/orl/omf/c/omfentr.c
+++ bld/orl/omf/c/omfentr.c
@@ -111,7 +111,7 @@ orl_return OMFENTRY OmfFileScan( omf_file_handle ofh, char *desired,
     assert( ofh );
     assert( func );
 
-    if( !desired ) {
+    if( desired == NULL ) {
         /* global request */
         sh = ofh->first_sec;
         while( sh ) {
@@ -130,7 +130,7 @@ orl_return OMFENTRY OmfFileScan( omf_file_handle ofh, char *desired,
         while( ds != NULL ) {
             sym = ds->data;
             if( ( sym->typ == ORL_SYM_TYPE_SECTION ) &&
-               !( sym->flags | OMF_SYM_FLAGS_GRPDEF ) ) {
+               !( sym->flags & OMF_SYM_FLAGS_GRPDEF ) ) {
                 err = func( (orl_sec_handle) sym->section );
                 if( err != ORL_OKAY ) return( err );
             }
