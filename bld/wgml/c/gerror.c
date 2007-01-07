c9c79048235accba4450f41f4c2c498617b160b8
Frank Beythien <N/A>
2007-01-07 20:08:53 +0100

    Missing file for wgml change 31186
    [git-p4: depot-paths = "//depot/openwatcom/": change = 31187]

---
+++ bld/wgml/c/gerror.c
@@ -0,0 +1,56 @@
+/****************************************************************************
+*
+*                            Open Watcom Project
+*
+*  Copyright (c) 2004-2007 The Open Watcom Contributors. All Rights Reserved.
+*
+*  ========================================================================
+*
+*    This file contains Original Code and/or Modifications of Original
+*    Code as defined in and that are subject to the Sybase Open Watcom
+*    Public License version 1.0 (the 'License'). You may not use this file
+*    except in compliance with the License. BY USING THIS FILE YOU AGREE TO
+*    ALL TERMS AND CONDITIONS OF THE LICENSE. A copy of the License is
+*    provided with the Original Code and Modifications, and is also
+*    available at www.sybase.com/developer/opensource.
+*
+*    The Original Code and all software distributed under the License are
+*    distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
+*    EXPRESS OR IMPLIED, AND SYBASE AND ALL CONTRIBUTORS HEREBY DISCLAIM
+*    ALL SUCH WARRANTIES, INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF
+*    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR
+*    NON-INFRINGEMENT. Please see the License for the specific language
+*    governing rights and limitations under the License.
+*
+*  ========================================================================
+*
+* Description:  Error and warning message output.
+*               Message definition and output to be improved
+****************************************************************************/
+
+
+
+#include "wgml.h"
+#include "gvars.h"
+#include <stdarg.h>
+
+
+
+void GSuicide( void )
+{
+    if( Environment ) {
+        longjmp( *Environment, 1 );
+    }
+    MyExit( 16 );
+}
+
+void OutMsg( char *msg, ... )
+/***************************/
+{
+    va_list args;
+
+    va_start( args, msg );
+    vprintf_s( msg, args );
+    va_end( args );
+}
+
