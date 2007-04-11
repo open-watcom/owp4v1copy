38fbb9a478898816677ca6d516d264caad4fb4fa
Michal Necasek <N/A>
2007-04-11 16:49:31 +0100

    Added/fixed a few comments.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 31405]

--- bld/trap/lcl/win/c/accmap.c
+++ bld/trap/lcl/win/c/accmap.c
@@ -24,10 +24,10 @@
 *
 *  ========================================================================
 *
-* Description:  WHEN YOU FIGURE OUT WHAT THIS FILE DOES, PLEASE
-*               DESCRIBE IT HERE!
+* Description:  Debuggee address mapping (using Toolhelp).
 *
 ****************************************************************************/
+
 
 #ifdef __WINDOWS__
 #pragma library("toolhelp.lib");
--- bld/trap/lcl/win/c/fault.c
+++ bld/trap/lcl/win/c/fault.c
@@ -24,8 +24,7 @@
 *
 *  ========================================================================
 *
-* Description:  WHEN YOU FIGURE OUT WHAT THIS FILE DOES, PLEASE
-*               DESCRIBE IT HERE!
+* Description:  Handle interrupts for trap file (called from asm wrapper).
 *
 ****************************************************************************/
 
