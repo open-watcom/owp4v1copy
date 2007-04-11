ff79848d767e053dca6c31f1e5cb995054838f55
Michal Necasek <N/A>
2007-04-11 12:21:10 +0100

    Added trivial signal()/raise() test.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 31401]

--- bld/clib/process/c/proctest.c
+++ bld/clib/process/c/proctest.c
@@ -33,6 +33,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <process.h>
+#include <signal.h>
 
 
 char ProgramName[128];                          /* executable filename */
@@ -91,6 +92,17 @@ int main( void )
 
 #define CHILD_RC        0x55
 
+/* Define data needed to test signal handling */
+sig_atomic_t signal_count;
+sig_atomic_t signal_number;
+
+
+void break_handler( int signo )
+{
+    signal_count++;
+    signal_number = signo;
+}
+
 
 /****
 ***** Program entry point.
@@ -110,7 +122,7 @@ int main( int argc, char * const argv[] )
 
     if( child ) {
         char    *env_var;
-	
+        
         /* Verify expected command line contents */
         VERIFY( argc == 4 );
         VERIFY( !strcmp( argv[1], ARG1 ) );
@@ -123,20 +135,20 @@ int main( int argc, char * const argv[] )
         VERIFY( !strcmp( env_var, VAR_TEXT ) );
 
         if( NumErrors != 0 ) {
-	    return( EXIT_FAILURE );
+            return( EXIT_FAILURE );
         } else {
             return( CHILD_RC );
         }
     } else {
         int         rc;
-	char        **env;
-	const char  *path = NULL;
-	const char  *child_args[] = { ProgramName, ARG1, ARG2, ARG3, NULL };
-	const char  *child_envp[] = { NULL, VAR_NAME "=" VAR_TEXT, "DOS4G=QUIET", NULL };
+        char        **env;
+        const char  *path = NULL;
+        const char  *child_args[] = { ProgramName, ARG1, ARG2, ARG3, NULL };
+        const char  *child_envp[] = { NULL, VAR_NAME "=" VAR_TEXT, "DOS4G=QUIET", NULL };
 
         /* We need to pass PATH down to the child because DOS/4GW style stub
-	 * programs rely on it to function properly.
-	 */
+         * programs rely on it to function properly.
+         */
         env = environ;
         while( env ) {
             if( !strncmp( *env, "PATH=", 5 ) ) {
@@ -163,7 +175,7 @@ int main( int argc, char * const argv[] )
 
         /* Modify our environment that child will inherit */
         VERIFY( !setenv( VAR_NAME, VAR_TEXT, 1 ) );
-	
+        
         rc = spawnl( P_WAIT, ProgramName, ProgramName, ARG1, ARG2, ARG3, NULL );
         VERIFY( rc == CHILD_RC );
 
@@ -175,6 +187,20 @@ int main( int argc, char * const argv[] )
 
         rc = spawnvp( P_WAIT, ProgramName, child_args );
         VERIFY( rc == CHILD_RC );
+
+        signal_count = 0;
+        signal_number = 0;
+        /* Install SIGBREAK handler */
+        VERIFY( signal( SIGBREAK, break_handler ) == SIG_DFL );
+
+        /* Raise signal and verify results */
+        VERIFY( raise( SIGBREAK ) == 0 );
+        VERIFY( signal_count == 1 );
+        VERIFY( signal_number == SIGBREAK );
+
+        /* Raise again - nothing should have happened */
+        VERIFY( raise( SIGBREAK ) == 0 );
+        VERIFY( signal_count == 1 );
 
         /*** Print a pass/fail message and quit ***/
         if( NumErrors != 0 ) {
