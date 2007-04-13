b82fbdc5cf20b5522745bd6c1bc4472dbda4f97a
Michal Necasek <N/A>
2007-04-13 10:01:34 +0100

    Removed obsolete files and replaced them with a readme.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 31418]

--- bld/wv/doc/ps.bat
+++
@@ -1,6 +0,0 @@
-@set gmllib=%doc_root%\gml\syslib;
-@set gmlinc=.;%doc_root%\doc\whelp;%doc_root%\doc\gml;
-@set gml_path=%doc_root%\gml\dos
-@%gml_path%\wgml wddoc (file ps.opt line out wddoc
-@set gmllib=
-@set gml_path=
--- bld/wv/doc/ps.cmd
+++
@@ -1,6 +0,0 @@
-@set gmllib=%doc_root%\gml\syslib;
-@set gmlinc=.;%doc_root%\doc\whelp;%doc_root%\doc\gml;
-@set gml_path=%doc_root%\gml\os2
-@%gml_path%\wgml wddoc (file ps.opt line out wddoc
-@set gmllib=
-@set gml_path=
