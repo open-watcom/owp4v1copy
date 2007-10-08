7daf530e3b17bcf83aa42e0d19187673e9fe93f5
David Golub <golubdr@gmail.com>
2007-10-08 21:33:23 +0100

    Added ddraw.mh to w32api2.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 32024]

---
+++ bld/w32api2/lib/strmiids.c
@@ -0,0 +1,74 @@
+/***************************************************************************
+*
+*                            Open Watcom Project
+*
+*  Copyright (c) 2004-2007 The Open Watcom Contributors. All Rights Reserved.
+*
+*  ========================================================================
+*
+*    This file contains Original Code and/or Modifications of Original
+*    Code as defined in and that are subject to the Sybase Open Watcom
+*    Public License version 1.0 (the 'License'). You may not use this file
+*    except in compliance with the License. BY USING THIS FILE YOU AGREE TO
+*    ALL TERMS AND CONDITIONS OF THE LICENSE. A copy of the License is
+*    provided with the Original Code and Modifications, and is also
+*    available at www.sybase.com/developer/opensource.
+*
+*    The Original Code and all software distributed under the License are
+*    distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
+*    EXPRESS OR IMPLIED, AND SYBASE AND ALL CONTRIBUTORS HEREBY DISCLAIM
+*    ALL SUCH WARRANTIES, INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF
+*    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR
+*    NON-INFRINGEMENT. Please see the License for the specific language
+*    governing rights and limitations under the License.
+*
+*  ========================================================================
+*
+*  Description: Implementation of the strmiids.lib library.
+*
+**************************************************************************/
+
+#include <guiddef.h>
+
+/* mmstream.h */
+EXTERN_C const IID  IID_IMultiMediaStream =
+    { 0xB502D1BC, 0x9A57, 0x11D0, { 0x8F, 0xDE, 0x00, 0xC0, 0x4F, 0xD9, 0x18, 0x9D } };
+EXTERN_C const IID  IID_IMediaStream =
+    { 0xB502D1BD, 0x9A57, 0x11D0, { 0x8F, 0xDE, 0x00, 0xC0, 0x4F, 0xD9, 0x18, 0x9D } };
+EXTERN_C const IID  IID_IStreamSample =
+    { 0xB502D1BE, 0x9A57, 0x11D0, { 0x8F, 0xDE, 0x00, 0xC0, 0x4F, 0xD9, 0x18, 0x9D } };
+
+/* ddraw.h */
+EXTERN_C const CLSID    CLSID_DirectDraw =
+    { 0xD7B70EE0, 0x4340, 0x11CF, { 0xB0, 0x63, 0x00, 0x20, 0xAF, 0xC2, 0xCD, 0x35 } };
+EXTERN_C const CLSID    CLSID_DirectDraw7 =
+    { 0x3C305196, 0x50DB, 0x11D3, { 0x9C, 0xFE, 0x00, 0xC0, 0x4F, 0xD9, 0x30, 0xC5 } };
+EXTERN_C const CLSID    CLSID_DirectDrawClipper =
+    { 0x593817A0, 0x7DB3, 0x11CF, { 0xA2, 0xDE, 0x00, 0xAA, 0x00, 0xB9, 0x33, 0x56 } };
+EXTERN_C const IID      IID_IDirectDraw =
+    { 0x6C14DB80, 0xA733, 0x11CE, { 0xA5, 0x21, 0x00, 0x20, 0xAF, 0x0B, 0xE5, 0x60 } };
+EXTERN_C const IID      IID_IDirectDraw2 =
+    { 0xB3A6F3E0, 0x2B43, 0x11CF, { 0xA2, 0xDE, 0x00, 0xAA, 0x00, 0xB9, 0x33, 0x56 } };
+EXTERN_C const IID      IID_IDirectDraw4 =
+    { 0x9C59509A, 0x39BD, 0x11D1, { 0x8C, 0x4A, 0x00, 0xC0, 0x4F, 0xD9, 0x30, 0xC5 } };
+EXTERN_C const IID      IID_IDirectDraw7 =
+    { 0x15E65EC0, 0x3B9C, 0x11D2, { 0xB9, 0x2F, 0x00, 0x60, 0x97, 0x97, 0xEA, 0x5B } };
+EXTERN_C const IID      IID_IDirectDrawSurface =
+    { 0x6C14DB81, 0xA733, 0x11CE, { 0xA5, 0x21, 0x00, 0x20, 0xAF, 0x0B, 0xE5, 0x60 } };
+EXTERN_C const IID      IID_IDirectDrawSurface2 =
+    { 0x57805885, 0x6EEC, 0x11CF, { 0x94, 0x41, 0xA8, 0x23, 0x03, 0xC1, 0x0E, 0x27 } };
+EXTERN_C const IID      IID_IDirectDrawSurface3 =
+    { 0xDA044E00, 0x69B2, 0x11D0, { 0xA1, 0xD5, 0x00, 0xAA, 0x00, 0xB8, 0xDF, 0xBB } };
+EXTERN_C const IID      IID_IDirectDrawSurface4 =
+    { 0x0B2B8630, 0xAD35, 0x11D0, { 0x8E, 0xA6, 0x00, 0x60, 0x97, 0x97, 0xEA, 0x5B } };
+EXTERN_C const IID      IID_IDirectDrawSurface7 =
+    { 0x06675A80, 0x3B9B, 0x11D2, { 0xB9, 0x2F, 0x00, 0x60, 0x97, 0x97, 0xEA, 0x5B } };
+EXTERN_C const IID      IID_IDirectDrawPalette =
+    { 0x6C14DB84, 0xA733, 0x11CE, { 0xA5, 0x21, 0x00, 0x20, 0xAF, 0x0B, 0xE5, 0x60 } };
+EXTERN_C const IID      IID_IDirectDrawClipper =
+    { 0x6C14DB85, 0xA733, 0x11CE, { 0xA5, 0x21, 0x00, 0x20, 0xAF, 0x0B, 0xE5, 0x60 } };
+EXTERN_C const IID      IID_IDirectDrawColorControl =
+    { 0x4B9F0EE0, 0x0D7E, 0x11D0, { 0x9B, 0x06, 0x00, 0xA0, 0xC9, 0x03, 0xA3, 0xB8 } };
+EXTERN_C const IID      IID_IDirectDrawGammaControl =
+    { 0x69C11C3E, 0xB46E, 0x11D1, { 0xAD, 0x7A, 0x00, 0xC0, 0x4F, 0xC2, 0x9B, 0x4E } };
+
