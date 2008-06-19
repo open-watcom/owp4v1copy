e2a7155288d8c8dc0dfe18263a0520d0b1109406
Jiri Malak <N/A>
2008-06-19 16:21:37 +0100

    Add awk a sed to builder tools.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 33009]

--- contrib/awk/ytab.c
+++ contrib/awk/ytab.c
@@ -3276,7 +3276,7 @@ yyerrlab:
 `---------------------------------------------------*/
 yyerrorlab:
 
-#ifdef __GNUC__
+#if defined( __GNUC__ ) || defined( __WATCOMC__ )
   /* Pacify GCC when the user code never invokes YYERROR and the label
      yyerrorlab therefore never appears in user code.  */
   if (0)
