6d7c60c1dce4820b669b2d124dcdea929ba6a50d
Jiri Malak <N/A>
2009-08-30 13:37:15 +0100

    Fix WATTCP for new WASM C naming convention.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 34978]

--- contrib/wattcp/src/asmpkt.h
+++ contrib/wattcp/src/asmpkt.h
@@ -2,7 +2,11 @@
 #define __ASMPKT_H
 
 #if (DOSX == 0)                                  /* for real-mode targets   */
+ #if __WATCOMC__ > 1280
+  extern void far cdecl pkt_receiver_rm (void);        /* in asmpkt.asm/asmpkt2.s */
+ #else
   extern void far pkt_receiver_rm (void);        /* in asmpkt.asm/asmpkt2.s */
+ #endif
 
 #elif defined(__WATCOMC__) && (DOSX & DOS4GW) || /* in asmpkt4.asm */ \
       defined(__BORLAND386__) && (DOSX == WDOSX)
@@ -13,6 +17,7 @@
   extern void pkt_receiver4_start();
   extern void pkt_receiver4_rm();
   extern void pkt_receiver4_end();
+
 #endif
 
 #endif
