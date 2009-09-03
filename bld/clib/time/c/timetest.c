e3b9092aab09c961b2b9b2273f70355cbf3a4da7
Michal Necasek <N/A>
2009-09-03 14:38:01 +0100

    Fixed up formatting.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 34998]

--- bld/clib/time/c/timetest.c
+++ bld/clib/time/c/timetest.c
@@ -28,6 +28,7 @@
 *
 ****************************************************************************/
 
+
 #include <errno.h>
 #include <locale.h>
 #include <stdio.h>
@@ -70,14 +71,15 @@ int main( int argc, char * const argv[] )
     char                buf[64];
     char                myfile[ sizeof __FILE__ ];
 
-    #ifdef __SW_BW
-    FILE *my_stdout;
+#ifdef __SW_BW
+    FILE                *my_stdout;
+
     my_stdout = freopen( "tmp.log", "a", stdout );
     if( my_stdout == NULL ) {
         fprintf( stderr, "Unable to redirect stdout\n" );
         exit( -1 );
     }
-    #endif
+#endif
     ( void ) argc;                      /* Unused */
     /*** Initialize ***/
     strcpy( ProgramName, argv[0] );     /* store filename */
@@ -200,42 +202,42 @@ int main( int argc, char * const argv[] )
     tm1.tm_isdst = -1;
     tt1 = mktime( &tm1 );
 
-    strftime(buf, sizeof (buf), "%a %A %b %B %c %C", &tm1);
-    VERIFY( 0 == strcmp(buf,"Sun Sunday Jun June Sun Jun 04 14:19:57 2006 20") );
+    strftime( buf, sizeof( buf ), "%a %A %b %B %c %C", &tm1 );
+    VERIFY( 0 == strcmp( buf, "Sun Sunday Jun June Sun Jun 04 14:19:57 2006 20" ) );
 
-    strftime(buf, sizeof (buf), "%d %D %e %E %f %F", &tm1);
-    VERIFY( 0 == strcmp(buf,"04 06/04/06  4  f 2006-06-04") );
+    strftime( buf, sizeof( buf ), "%d %D %e %E %f %F", &tm1 );
+    VERIFY( 0 == strcmp( buf, "04 06/04/06  4  f 2006-06-04" ) );
 
-    strftime(buf, sizeof (buf), "%g %G %h %H %i %I", &tm1);
-    VERIFY( 0 == strcmp(buf,"06 2006 Jun 14 i 02") );
+    strftime( buf, sizeof( buf ), "%g %G %h %H %i %I", &tm1 );
+    VERIFY( 0 == strcmp( buf, "06 2006 Jun 14 i 02" ) );
 
-    strftime(buf, sizeof (buf), "%j %J %k %K %l %L", &tm1);
-    VERIFY( 0 == strcmp(buf,"155 J k K l L") );
+    strftime( buf, sizeof( buf ), "%j %J %k %K %l %L", &tm1 );
+    VERIFY( 0 == strcmp( buf, "155 J k K l L" ) );
 
-    strftime(buf, sizeof (buf), "%m %M %n %N %o %O", &tm1);
-    VERIFY( 0 == strcmp(buf,"06 19 \n N o ") );
+    strftime( buf, sizeof( buf ), "%m %M %n %N %o %O", &tm1 );
+    VERIFY( 0 == strcmp( buf, "06 19 \n N o " ) );
 
-    strftime(buf, sizeof (buf), "%p %P %q %Q %r %R", &tm1);
-    VERIFY( 0 == strcmp(buf,"PM P q Q 02:19:57 PM 14:19") );
+    strftime( buf, sizeof( buf ), "%p %P %q %Q %r %R", &tm1 );
+    VERIFY( 0 == strcmp( buf, "PM P q Q 02:19:57 PM 14:19" ) );
 
-    strftime(buf, sizeof (buf), "%s %S %t %T %u %U", &tm1);
-    VERIFY( 0 == strcmp(buf,"s 57 \t 14:19:57 7 23") );
+    strftime( buf, sizeof( buf ), "%s %S %t %T %u %U", &tm1 );
+    VERIFY( 0 == strcmp( buf, "s 57 \t 14:19:57 7 23" ) );
 
-    strftime(buf, sizeof (buf), "%v %V %w %W %x %X", &tm1);
-    VERIFY( 0 == strcmp(buf,"v 22 0 22 Sun Jun 04, 2006 14:19:57") );
+    strftime( buf, sizeof( buf ), "%v %V %w %W %x %X", &tm1 );
+    VERIFY( 0 == strcmp( buf, "v 22 0 22 Sun Jun 04, 2006 14:19:57" ) );
 
 /*  %z %Z timezone dependant codes omitted */
-    strftime(buf, sizeof (buf), "%y %Y %%", &tm1);
-    VERIFY( 0 == strcmp(buf,"06 2006 %") );
+    strftime( buf, sizeof( buf ), "%y %Y %%", &tm1 );
+    VERIFY( 0 == strcmp( buf, "06 2006 %" ) );
 
-    strftime(buf, sizeof (buf), "%Ec %Ex %EY", &tm1);
-    VERIFY( 0 == strcmp(buf,"Sun Jun 04 14:19:57 2006 Sun Jun 04, 2006 2006") );
+    strftime( buf, sizeof( buf ), "%Ec %Ex %EY", &tm1 );
+    VERIFY( 0 == strcmp( buf, "Sun Jun 04 14:19:57 2006 Sun Jun 04, 2006 2006" ) );
 
-    strftime(buf, sizeof (buf), "%Od %Oe %OH %OI %OM %OS", &tm1);
-    VERIFY( 0 == strcmp(buf,"04  4 14 02 19 57") );
+    strftime( buf, sizeof( buf ), "%Od %Oe %OH %OI %OM %OS", &tm1 );
+    VERIFY( 0 == strcmp( buf, "04  4 14 02 19 57" ) );
 
-    strftime(buf, sizeof (buf), "%Ou %OU %OV %Ow %OW %Oy", &tm1);
-    VERIFY( 0 == strcmp(buf,"7 23 22 0 22 06") );
+    strftime( buf, sizeof( buf ), "%Ou %OU %OV %Ow %OW %Oy", &tm1 );
+    VERIFY( 0 == strcmp( buf, "7 23 22 0 22 06" ) );
 
 
     /*** Print a pass/fail message and quit ***/
@@ -244,12 +246,10 @@ int main( int argc, char * const argv[] )
         return( EXIT_FAILURE );
     }
     printf( "Tests completed (%s).\n", ProgramName );
-    #ifdef __SW_BW
-    {
-        fprintf( stderr, "Tests completed (%s).\n", ProgramName );
-        fclose( my_stdout );
-        _dwShutDown();
-    }
-    #endif
+#ifdef __SW_BW
+    fprintf( stderr, "Tests completed (%s).\n", ProgramName );
+    fclose( my_stdout );
+    _dwShutDown();
+#endif
     return( 0 );
 }
