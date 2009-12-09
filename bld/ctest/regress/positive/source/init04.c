9cd1fe42c176487b02aa0d52af3ddec73fd5d1ef
Michal Necasek <N/A>
2009-12-09 15:42:34 +0100

    Modified test which was making incorrect assumptions.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 35412]

--- bld/ctest/regress/positive/source/init04.c
+++ bld/ctest/regress/positive/source/init04.c
@@ -12,14 +12,16 @@ struct s
     };
 };
 
-struct s d = { 0.0, 0.1 };
+/* NB: If 0.1 is used instead of 0.25, the comparison may fail due
+ * to a precision loss caused by a double -> float -> double conversion.
+ */
+struct s d = { 0.0, 0.25 };
 
 main(void)
 {
     if( d.a != d.c[0] ) _fail;
     if( d.b != d.c[1] ) _fail;
     if( d.a != 0.0 ) _fail;
-    if( d.b != 0.1 ) _fail;
+    if( d.b != 0.25 ) _fail;
     _PASS;
 }
-
