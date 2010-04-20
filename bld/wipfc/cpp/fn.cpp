92140ffe9d86efb6d71a0fcade7f7d2e407b8a42
Dr. Lawrence W. Haynes <N/A>
2010-04-20 03:45:22 +0100

    Allow lists in footnotes
    [git-p4: depot-paths = "//depot/openwatcom/": change = 36228]

--- bld/wipfc/cpp/fn.cpp
+++ bld/wipfc/cpp/fn.cpp
@@ -47,13 +47,15 @@ Lexer::Token Fn::parse( Lexer* lexer )
     Lexer::Token tok( parseAttributes( lexer ) );
     bool done( false );
     while( tok != Lexer::END && !( tok == Lexer::TAG && lexer->tagId() == Lexer::EUSERDOC)) {
-        if( parseInline( lexer, tok ) ) {
-            if( lexer->tagId() == Lexer::EFN ) {
-                tok = Tag::parseAttributes( lexer );
-                break;
+        if( lexer->tagId() == Lexer::EFN ) {
+            tok = Tag::parseAttributes( lexer );
+            break;
+        }
+        else if( parseInline( lexer, tok ) ) {
+            if( parseBlock( lexer, tok ) ) {
+                if( parseListBlock( lexer, tok ) )
+                    parseCleanup( lexer, tok );
             }
-            else if( parseBlock( lexer, tok ) )
-                parseCleanup( lexer, tok );
         }
     }
     return tok;
