b533ec4dbaef59ce50478a1b3a67bcadecad0e89
Michal Necasek <N/A>
2010-05-10 09:17:15 +0100

    Removed a forgotten fnrstor (there's no such instruction).
    [git-p4: depot-paths = "//depot/openwatcom/": change = 36300]

--- bld/trap/lcl/asm/misc7086.asm
+++ bld/trap/lcl/asm/misc7086.asm
@@ -162,11 +162,7 @@ Write8087 PROC
         push    bx
         mov     ds,dx
         mov     bx,ax
-ifdef REAL_MODE
-        fnrstor [bx]
-else
         frstor  [bx]
-endif
         fwait
         pop     bx
         pop     ds
@@ -176,7 +172,7 @@ Write8087 ENDP
 comment ~
     These routines read/write the FPU or emulator state when we're on a 386
     or better. They're a bit wierd so here's a explanation. There are operand
-    size overrides on the fnsave and fnrstor so that a real FPU will save the
+    size overrides on the fnsave and frstor so that a real FPU will save the
     full 32-bit state (this code is in a 16-bit segment). The emulator does
     not respect the operand size, but since it always stores the full state
     anyway that's OK. However, the 32-bit emulator does not know how to decode
