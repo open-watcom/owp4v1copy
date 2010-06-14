9af11237eb939485b335ba7f3b98204e6979fb59
Michal Necasek <N/A>
2010-06-14 13:43:23 +0100

    Added a note about several not so obvious wasm FPU details.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 36406]

--- bld/wasm/test/test3/fpu1.asm
+++ bld/wasm/test/test3/fpu1.asm
@@ -1,3 +1,17 @@
+; Notes on the FPU tests:
+;
+; - By default, wasm generates code with support for 8087 software emulation
+;   (-fpi). That is different from MASM/TASM which default to no emulation.
+;   It's not clear why wasm is different in this respect.
+; - When 8087 emulation is used, FWAIT is prefixed by a NOP. Additionally,
+;   FRSTOR and FLDENV and all non-control FPU instructions get a WAIT prefix
+;   even on 286 and later.
+; - When emulation is not used (-fpi87) and 286 or higher CPU is selected,
+;   FRSTOR, FLDENV and non-control FPU instructions get no WAIT prefix. Note
+;   that this behavior depends on the CPU, not FPU selection, since the CPU
+;   type determines how the FPU is connected (and combinations like .386 with
+;   .8087 are technically impossible).
+
 .8086
 .8087
 
