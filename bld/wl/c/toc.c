27d5b94539c927bb26ba15fe5b560a6963e9e764
Jiri Malak <N/A>
2010-06-19 11:17:48 +0100

    Reformat source code.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 36423]

--- bld/wl/c/toc.c
+++ bld/wl/c/toc.c
@@ -82,14 +82,14 @@ void ResetToc( void )
 void CleanToc( void )
 /**************************/
 {
-    ZapHTable(Toc, LFree);
+    ZapHTable( Toc, LFree );
 }
 
 static unsigned TocEntryHashFunc( void *_e, unsigned size )
 /*********************************************************/
 {
     TocEntry *e = _e;
-    return DataHashFunc(&e->e, sizeof e->e, size);
+    return( DataHashFunc( &e->e, sizeof( e->e ), size ) );
 }
 
 static int TocEntryCmp( const void *_e1, const void *_e2 )
@@ -97,10 +97,10 @@ static int TocEntryCmp( const void *_e1, const void *_e2 )
 {
     const TocEntry *e1 = _e1;
     const TocEntry *e2 = _e2;
-    return memcmp(&e1->e, &e2->e, sizeof e1->e);
+    return( memcmp( &e1->e, &e2->e, sizeof( e1->e ) ) );
 }
 
-void InitToc(void)
+void InitToc( void )
 /***********************/
 {
     Toc = CreateHTable( 1024, TocEntryHashFunc, TocEntryCmp, ChkLAlloc, LFree );
@@ -119,7 +119,7 @@ void CheckIfTocSym( symbol *sym )
 /**************************************/
 {
     if( TocName != NULL && TocSym == NULL ) {
-        if( strcmp(sym->name, TocName) == 0 ) {
+        if( strcmp( sym->name, TocName ) == 0 ) {
             TocSym = sym;
         }
     }
@@ -128,7 +128,7 @@ void CheckIfTocSym( symbol *sym )
 bool IsTocSym( symbol *sym )
 /*********************************/
 {
-    return TocSym == sym;
+    return( TocSym == sym );
 }
 
 static void AddToToc( TocEntryId *e )
@@ -139,12 +139,12 @@ static void AddToToc( TocEntryId *e )
 
     searchEntry.e = *e;
 
-    if( FindHTableElem(Toc, &searchEntry) == NULL ) {
-        entry = ChkLAlloc(sizeof *entry);
+    if( FindHTableElem( Toc, &searchEntry ) == NULL ) {
+        entry = ChkLAlloc( sizeof( *entry ) );
         entry->e = *e;
         entry->pos = TocSize;
-        TocSize += sizeof(offset);
-        AddHTableElem(Toc, entry);
+        TocSize += sizeof( offset );
+        AddHTableElem( Toc, entry );
     }
 }
 
@@ -155,7 +155,7 @@ void AddSymToToc( symbol *sym )
 
     e.sdata = NULL;
     e.u.sym = sym;
-    AddToToc(&e);
+    AddToToc( &e );
 }
 
 void AddSdataOffToToc( segdata *sdata, offset off )
@@ -166,7 +166,7 @@ void AddSdataOffToToc( segdata *sdata, offset off )
     DbgAssert( sdata != NULL );
     e.sdata = sdata;
     e.u.off = off;
-    AddToToc(&e);
+    AddToToc( &e );
 }
 
 
@@ -179,7 +179,7 @@ static void ConvertTocEntryId( TocEntryId *e )
         symbol *sym = e->u.sym;
         segdata *seg = sym->p.seg;
 
-        if( IS_SYM_IMPORTED(sym) || seg == NULL ) {
+        if( IS_SYM_IMPORTED( sym ) || seg == NULL ) {
             // do not convert; keep symbol around
         } else {
             e->sdata = seg;
@@ -190,19 +190,19 @@ static void ConvertTocEntryId( TocEntryId *e )
     return;
 }
 
-static void ConvertTocEntry( void *e)
-/***********************************/
+static void ConvertTocEntry( void *e )
+/************************************/
 {
-    ConvertTocEntryId(&((TocEntry *)e)->e);
+    ConvertTocEntryId( &((TocEntry *)e)->e );
 }
 
-#define GOT_RESERVED_NEG_SIZE (2* sizeof(long))
-#define GOT_RESERVED_POS0_SIZE (3* sizeof(long))
-#define GOT_RESERVED_SIZE (GOT_RESERVED_NEG_SIZE+GOT_RESERVED_POS0_SIZE)
+#define GOT_RESERVED_NEG_SIZE (2 * sizeof( long ))
+#define GOT_RESERVED_POS0_SIZE (3 * sizeof( long ))
+#define GOT_RESERVED_SIZE (GOT_RESERVED_NEG_SIZE + GOT_RESERVED_POS0_SIZE)
 
 #if 0 // OS/2 PPC development temporarly on hold
-static void AdjustGotEntry( TocEntry *e, offset *middle) {
-    if( e->pos < *middle) {
+static void AdjustGotEntry( TocEntry *e, offset *middle ) {
+    if( e->pos < *middle ) {
         e->pos -= GOT_RESERVED_NEG_SIZE;
     } else {
         e->pos += GOT_RESERVED_POS0_SIZE;
@@ -213,19 +213,20 @@ static void AdjustGotEntry( TocEntry *e, offset *middle) {
 void PrepareToc( void )
 /****************************/
 {
-    if( Toc == NULL ) return;
-    WalkHTable(Toc, ConvertTocEntry);
-    RehashHTable(Toc);
+    if( Toc == NULL )
+        return;
+    WalkHTable( Toc, ConvertTocEntry );
+    RehashHTable( Toc );
     if( IS_PPC_OS2 ) {
         // Development temporarly on hold
         #if 0
-        offset middle = (TocSize/2) & ~0x3;
-        WalkHTableCookie(Toc, AdjustGotEntry, &middle);
-        TocShift = middle+GOT_RESERVED_NEG_SIZE;
+        offset middle = ( TocSize / 2 ) & ~0x3;
+        WalkHTableCookie( Toc, AdjustGotEntry, &middle );
+        TocShift = middle + GOT_RESERVED_NEG_SIZE;
         TocSize += GOT_RESERVED_SIZE;
         #endif
     }
-    if( TocSym != NULL)  {
+    if( TocSym != NULL )  {
         TocSym->info |= SYM_DCE_REF | SYM_DEFINED;
         SetAddPubSym( TocSym, SYM_REGULAR, FakeModule, 0, 0 );
         if( LinkFlags & STRIP_CODE ) {
@@ -237,7 +238,8 @@ void PrepareToc( void )
 void SetTocAddr( offset off, group_entry *group )
 /******************************************************/
 {
-    if( Toc == NULL || TocSym == NULL ) return;
+    if( Toc == NULL || TocSym == NULL )
+        return;
     XDefSymAddr( TocSym, off + TocShift, group->grp_addr.seg );
 }
 
@@ -247,10 +249,10 @@ static signed_32 OffFromToc( offset off )
     offset toff;
 
     toff = off - TocShift;
-    if( ((signed_16) toff) != (signed_32) toff ) {
+    if( (signed_16)toff != (signed_32)toff ) {
         LnkMsg( ERR+MSG_TOC_TOO_BIG, NULL );
     }
-    return toff;
+    return( toff );
 }
 
 signed_32 FindEntryPosInToc( TocEntryId *e )
@@ -260,11 +262,11 @@ signed_32 FindEntryPosInToc( TocEntryId *e )
     TocEntry *  entry;
 
     searchEntry.e = *e;
-    entry = FindHTableElem(Toc, &searchEntry);
-    if (entry != NULL) {
-        return OffFromToc(entry->pos);
+    entry = FindHTableElem( Toc, &searchEntry );
+    if( entry != NULL ) {
+        return( OffFromToc( entry->pos ) );
     } else {
-        return BOGUS; // return bogus position; likely to cause alignment exception
+        return( BOGUS ); // return bogus position; likely to cause alignment exception
     }
 
 }
@@ -277,7 +279,7 @@ signed_32 FindSdataOffPosInToc( segdata *sdata, offset off )
     DbgAssert( sdata != NULL );
     e.sdata = sdata;
     e.u.off = off;
-    return FindEntryPosInToc(&e);
+    return( FindEntryPosInToc( &e ) );
 }
 
 signed_32 FindSymPosInToc( symbol * sym )
@@ -287,8 +289,8 @@ signed_32 FindSymPosInToc( symbol * sym )
 
     e.sdata = NULL;
     e.u.sym = sym;
-    ConvertTocEntryId(&e);
-    return FindEntryPosInToc(&e);
+    ConvertTocEntryId( &e );
+    return( FindEntryPosInToc( &e ) );
 }
 
 static void WriteOutTokElem( void *_elem, void *buf )
@@ -305,29 +307,30 @@ static void WriteOutTokElem( void *_elem, void *buf )
         addr = elem->e.u.off + sdata->a.delta + leader->group->linear
                         + leader->seg_addr.off +  FmtData.base;
     } else {
-        addr = SymbolAbsAddr(elem->e.u.sym);
+        addr = SymbolAbsAddr( elem->e.u.sym );
     }
-    DbgAssert(elem->pos >= 0);
-    PutInfo((*((virt_mem *)buf)) + elem->pos, &(addr), sizeof addr);
+    DbgAssert( elem->pos >= 0 );
+    PutInfo( (*((virt_mem *)buf)) + elem->pos, &addr, sizeof( addr ) );
 }
 
 void WriteToc( virt_mem buf )
 /**********************************/
 {
-    if( Toc == NULL ) return;
+    if( Toc == NULL )
+        return;
     WalkHTableCookie( Toc, WriteOutTokElem, &buf );
     if( IS_PPC_OS2 ) {
         // Development temporarly on hold
         #if 0
-        offset res[GOT_RESERVED_SIZE/sizeof(offset)] = { 0 };
-        enum { zero = GOT_RESERVED_NEG_SIZE/sizeof(offset) };
+        offset res[GOT_RESERVED_SIZE / sizeof( offset )] = { 0 };
+        enum { zero = GOT_RESERVED_NEG_SIZE / sizeof( offset ) };
         enum { blrl_opcode = 0x4E800021 };
 
-        DbgAssert(TocShift >= GOT_RESERVED_NEG_SIZE);
+        DbgAssert( TocShift >= GOT_RESERVED_NEG_SIZE );
 
-        res[zero-1] = blrl_opcode;
+        res[zero - 1] = blrl_opcode;
         res[zero] = IDataGroup->linear + FmtData.base;
-        PutInfo(buf+TocShift-GOT_RESERVED_NEG_SIZE, res, GOT_RESERVED_SIZE);
+        PutInfo( buf + TocShift - GOT_RESERVED_NEG_SIZE, res, GOT_RESERVED_SIZE );
         #endif
     }
 }
