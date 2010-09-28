b0b9b9e61d39e8cf839be83f58caecbf7c801f3b
Leif Ekblad <leif@rdos.net>
2010-09-28 14:55:37 +0100

    Some updates to RDOS device executables.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 36781]

--- bld/wl/c/loadrdv.c
+++ bld/wl/c/loadrdv.c
@@ -47,24 +47,109 @@
 #include "dbgall.h"
 #include "loadrdv.h"
 
+static unsigned_16  CodeSeg = 0;
+static unsigned_16  DataSeg = 0;
+static unsigned_16  CodeSize = 0;
+static unsigned_16  DataSize = 0;
+static int          CodePos = 0;
+static int          DataPos = 0;
+
+static bool RelocWalkFn( void *data, unsigned_32 size, void *ctx )
+/****************************************************************/
+{
+    int                 pos;
+    int                 i;
+    dos_addr            *reloc = (dos_addr *)data;
+//    unsigned_16         seg;
+
+    for( i = size; i > 0; i -= sizeof(dos_addr) ) {
+        pos = 0;
+        if( reloc->seg == CodeSeg )
+            pos = CodePos;
+        if( reloc->seg == DataSeg )
+            pos = DataPos;
+        if( pos ) {
+            pos += reloc->off;
+            Root->outfile->file_loc = pos;
+/*            SeekLoad( pos );            
+            ReadLoad( &seg, sizeof(seg) );
+            if( seg == CodeSeg ) {
+                seg = RdosCodeSel;
+                SeekLoad( pos );
+                WriteLoad( &seg, sizeof(seg) );
+            }
+            if( seg == DataSeg ) {
+                seg = RdosDataSel;
+                SeekLoad( pos );            
+                WriteLoad( &seg, sizeof(seg) );
+            } */
+        }
+        reloc++;
+    }
+
+    return( FALSE );   /* don't stop walking */
+}
+
 static unsigned long WriteRDOSData( unsigned_32 hdr_size )
 /**********************************************************/
 /* copy code from extra memory to loadfile */
 {
     group_entry         *group;
+    SECTION             *sect;
+    RELOC_INFO          *relocs;
+    struct seg_leader   *leader;
+    SEGDATA             *piece;
+    int                 iscode;
+    int                 isdata;
     unsigned_32         file_size = 0;
 
     DEBUG(( DBG_BASE, "Writing data" ));
+    OrderGroups( CompareDosSegments );
+    CurrSect = Root;        // needed for WriteInfo.
 
     Root->outfile->file_loc = Root->u.file_loc;
     Root->sect_addr = Groups->grp_addr;
+    leader = 0;
 
+/* write groups and relocations */
     for( group = Groups; group != NULL; ) {
-        CurrSect = group->section;
-        file_size += group->size;
+        if( leader != group->leaders ) {
+            iscode = 0;
+            isdata = 0;
+            leader = group->leaders;
+            if( leader && leader->size ) {
+                piece = leader->pieces; 
+                if( piece ) {
+                    if( piece->iscode ) {
+                        CodeSeg = leader->seg_addr.seg;
+                        CodePos = Root->outfile->file_loc;
+                        iscode = 1;
+                    }
+                    if( piece->isidata || piece->isuninit ) {
+                        DataSeg = leader->seg_addr.seg;
+                        DataPos = Root->outfile->file_loc;
+                        isdata = 1;
+                    }
+                }
+            }
+        }
+        sect = group->section;
+        CurrSect = sect;
         WriteDOSGroup( group );
+        if( group->totalsize > group->size )
+            PadLoad( group->totalsize - group->size );
+        file_size += group->totalsize;
+        if( iscode )
+            CodeSize += group->totalsize;
+        if( isdata )
+            DataSize += group->totalsize;
         group = group->next_group;
     }
+
+    relocs = Root->reloclist;
+    if( Root->relocs )
+        WalkRelocList( &relocs, RelocWalkFn, 0 );
+
     return( file_size );
 }
 
