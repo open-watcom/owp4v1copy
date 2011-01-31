793f9e2e819c33d63556f71546f46e3af721cbd6
Leif Ekblad <leif@rdos.net>
2011-01-31 18:50:20 +0100

    Added basic kernel-mode include files for RDOS.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 36893]

---
+++ bld/watcom/h/rdosdev.h
@@ -0,0 +1,31 @@
+/****************************************************************************
+*
+*                            Open Watcom Project
+*
+*  Copyright (c) 2009-2010 The Open Watcom Contributors. All Rights Reserved.
+*
+*  ========================================================================
+*
+* Description:  RDOS kernel-mode header file
+*
+****************************************************************************/
+
+
+#ifndef _RDOSDEV_H
+#define _RDOSDEV_H
+
+#pragma pack( push, 1 )
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "rdk.h"
+
+// headers
+
+// aux definitions
+
+#pragma pack( pop )
+
+#endif
