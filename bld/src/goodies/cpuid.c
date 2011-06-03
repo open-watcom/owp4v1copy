aaad60c6e69d718654b41fda4bfb2253062ca04d
Michal Necasek <N/A>
2011-06-03 15:05:28 +0100

    More work on CPUID example.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 36962]

--- bld/src/goodies/cpuid.c
+++ bld/src/goodies/cpuid.c
@@ -2,7 +2,7 @@
  * cpuid.c - A sample program showing a method to identify CPU features
  * using the processor's CPUID instruction.
  *
- * For 16-bit or 32-bit environments.
+ * For 16-bit or 32-bit environments. A 32-bit CPU is required.
  */
 
 #include <stdio.h>
@@ -10,6 +10,22 @@
 #include <string.h>
 #include <stdint.h>
 #include <unistd.h>
+
+/* Miscellaneous constants */
+#define EFL_CPUID           (1L << 21)      /* The CPUID bit in EFLAGS. */
+
+#define CPUID_EAX           0               /* Index of EAX value in array. */
+#define CPUID_EBX           1               /* Index of EBX value in array. */
+#define CPUID_ECX           2               /* Index of ECX value in array. */
+#define CPUID_EDX           3               /* Index of EDX value in array. */
+
+#define CPUID_STD_BASE      0x00000000      /* Standard leaves base. */
+#define CPUID_HV_BASE       0x40000000      /* Hypervisor leaves base. */
+#define CPUID_EXT_BASE      0x80000000      /* Extended leaves base. */
+#define CPUID_CENTAUR_BASE  0xC0000000      /* Centaur leaves base. */
+#define CPUID_BRAND_1ST     0x80000002      /* First brand string leaf. */
+#define CPUID_BRAND_COUNT   3               /* Number of brand leaves. */
+
 
 /* Read the EFLAGS register. */
 uint32_t eflags_read( void );
@@ -147,39 +163,68 @@ static const char   *feature_ecx_desc[][2] =
     { "Advanced Vector Extensions", "AVX" },
     { "Reserved 3", "R3!!" },
     { "Reserved 4", "R4!!" },                   /* Bit 30 */
-    { "Hypervisor Present", "HP" }
+    { "Hypervisor Present", "HVP" }
 };
 
+/* Determine the number of CPUID leaves available for basic/extended/etc.
+ * CPUID. Due to poor design, it is difficult to reliably detect certain
+ * unimplemented leaves. We assume that at least two leaves must be present
+ * for a range to be valid.
+ */
+int cpuid_get_leaf_count( uint32_t base )
+{
+    uint32_t    cpu_info[4];
+    uint32_t    leaf_count;
+
+    cpu_id( cpu_info, base );
+    leaf_count = cpu_info[CPUID_EAX] > base ?
+                 cpu_info[CPUID_EAX] - base : 0;
+
+    /* A result higher than 1024 is considered garbage. */
+    return( leaf_count > 1024 ? 0 : leaf_count );
+}
+
+/* Dump available CPUID leaves in a machine-readable format. */
 void dump_raw_cpuid( void )
 {
     uint32_t    cpu_info[4];
-    uint32_t    max_id;
-    uint32_t    i;
+    uint32_t    base;
+    int         max_id;
+    int         i, j;
 
-    /* Obtain the number of standard CPUID leaves. */
-    cpu_id( cpu_info, 0 );
-    max_id = cpu_info[0];
-
-    printf( "CPUID Information (%d leaves):\n", max_id + 1 );
-    for( i = 0; i <= max_id; ++i ) {
-        cpu_id( cpu_info, i );
-        printf( "Leaf %02ld: %08lx %08lx %08lx %08lx\n", i,
-                cpu_info[0], cpu_info[1], cpu_info[2], cpu_info[3] );
-    }
-
-    /* Obtain the number of Extended CPUID leaves. */
-    cpu_id( cpu_info, 0x80000000 );
-    max_id = cpu_info[0] ? cpu_info[0] - 0x80000000 : 0;
-
-    if( max_id ) {
-        printf( "Extended CPUID Information (%d leaves):\n", max_id + 1 );
-        for( i = 0; i <= max_id; ++i ) {
-            cpu_id( cpu_info, 0x80000000 + i );
-            printf( "Ext Leaf %02ld: %08lx %08lx %08lx %08lx\n", i,
-                    cpu_info[0], cpu_info[1], cpu_info[2], cpu_info[3] );
+    for( i = 0; i < 4; ++i ) {
+        base = (uint32_t)i << 30;
+        max_id = cpuid_get_leaf_count( base );
+        if( max_id ) {
+            for( j = 0; j <= max_id; ++j ) {
+                cpu_id( cpu_info, base + j );
+                printf( "%08lx: %08lx %08lx %08lx %08lx\n", base + j,
+                        cpu_info[CPUID_EAX], cpu_info[CPUID_EBX],
+                        cpu_info[CPUID_ECX], cpu_info[CPUID_EDX] );
+            }
         }
-    } else {
-        printf( "No Extended CPUID Information.\n" );
+    }
+}
+
+/* Print the hypervisor identification, if present. */
+void print_hyper_id( int verbose )
+{
+    uint32_t    cpu_info[4];
+    uint32_t    max_hv_id;
+    char        vendor_string[3 * 4];
+    char        intrfc_string[3 * 4];
+
+    max_hv_id = cpuid_get_leaf_count( CPUID_HV_BASE );
+    if( max_hv_id ) {
+        cpu_id( cpu_info, CPUID_HV_BASE );
+        *(uint32_t *)(vendor_string + 0) = cpu_info[CPUID_EBX];
+        *(uint32_t *)(vendor_string + 4) = cpu_info[CPUID_ECX];
+        *(uint32_t *)(vendor_string + 8) = cpu_info[CPUID_EDX];
+        /* Note that the signature strings may not be null-terminated. */
+        printf( "Hypervisor signature: %.12s", vendor_string );
+        cpu_id( cpu_info, CPUID_HV_BASE + 1 );
+        *(uint32_t *)intrfc_string = cpu_info[CPUID_EAX];
+        printf( "  interface: %.4s", intrfc_string );
     }
 }
 
@@ -189,30 +234,30 @@ void print_cpuid( int verbose )
     uint32_t    max_id;
     uint32_t    max_ext_id;
     uint32_t    i;
-    uint32_t    features;
-    uint32_t    ext_feat;
-    char        cpu_vendor[32];
+    uint32_t    features_edx;
+    uint32_t    features_ecx;
+    char        cpu_vendor[16];
     char        brand_string[12 * 4];
 
     /* Obtain the number of CPUID leaves and the vendor string. */
     cpu_id( cpu_info, 0 );
-    max_id = cpu_info[0];
+    max_id = cpu_info[CPUID_EAX];
     memset( cpu_vendor, 0, sizeof( cpu_vendor ) );
-    *(uint32_t *)(cpu_vendor + 0) = cpu_info[1];
-    *(uint32_t *)(cpu_vendor + 4) = cpu_info[3];
-    *(uint32_t *)(cpu_vendor + 8) = cpu_info[2];
+    *(uint32_t *)(cpu_vendor + 0) = cpu_info[CPUID_EBX];
+    *(uint32_t *)(cpu_vendor + 4) = cpu_info[CPUID_EDX];
+    *(uint32_t *)(cpu_vendor + 8) = cpu_info[CPUID_ECX];
     printf( "CPU vendor string: %s\n", cpu_vendor );
 
     /* Obtain the model and features. */
     cpu_id( cpu_info, 1 );
-    features = cpu_info[3];
-    ext_feat = cpu_info[2];
+    features_edx = cpu_info[CPUID_EDX];
+    features_ecx = cpu_info[CPUID_ECX];
 
     /* Interpret the feature bits. */
 
     printf( "Processor features:\n" );
     for( i = 0; i < 32; ++i ) {
-        if( features & (1L << i) ) {
+        if( features_edx & (1L << i) ) {
             if( verbose )
                 printf( "%s (%s)\n", feature_edx_desc[i][0],
                         feature_edx_desc[i][1] );
@@ -220,11 +265,8 @@ void print_cpuid( int verbose )
                 printf( "%s ", feature_edx_desc[i][1] );
         }
     }
-    if (!verbose )
-        printf( "\n" );
-    printf( "Extended processor features:\n" );
     for( i = 0; i < 32; ++i ) {
-        if( ext_feat & (1L << i) ) {
+        if( features_ecx & (1L << i) ) {
             if( verbose )
                 printf( "%s (%s)\n", feature_ecx_desc[i][0],
                         feature_ecx_desc[i][1] );
@@ -232,35 +274,38 @@ void print_cpuid( int verbose )
                 printf( "%s ", feature_ecx_desc[i][1] );
         }
     }
-    if (!verbose )
+    if( !verbose )
         printf( "\n" );
 
     /* Obtain the brand string, if available. */
-    cpu_id( cpu_info, 0x80000000 );
-    max_ext_id = cpu_info[0] ? cpu_info[0] - 0x80000000 : 0;
+    max_ext_id = cpuid_get_leaf_count( CPUID_EXT_BASE );
     if( max_ext_id > 4 ) {
-        for( i = 0; i < 3; ++i ) {
-            cpu_id( cpu_info, 0x80000002 + i );
+        for( i = 0; i < CPUID_BRAND_COUNT; ++i ) {
+            cpu_id( cpu_info, CPUID_BRAND_1ST + i );
             memcpy( brand_string + i * sizeof( cpu_info ), 
                     cpu_info, sizeof( cpu_info ) );
         }
         /* Note that the brand string is null-terminated. */
         printf( "CPU brand string: %s\n", brand_string );
     }
+
+    /* Print the hypervisor signature, if any. */
+    print_hyper_id( verbose );
 }
 
-#define EFL_CPUID   (1L << 21)
-
-/* Check if CPUID instruction is supported. */
+/* Check if CPUID instruction is supported. Some CPUs without CPUID support
+ * may not raise an invalid opcode exception when CPUID is executed.
+ */
 int have_cpu_id( void )
 {
-    uint32_t    eflg;
+    uint32_t    old_eflg;
+    uint32_t    new_eflg;
 
-    eflg = eflags_read();
-    eflg |= EFL_CPUID;
-    eflags_write( eflg );
-    eflg = eflags_read();
-    return( !!(eflg & EFL_CPUID) );
+    old_eflg = eflags_read();
+    new_eflg = old_eflg ^ EFL_CPUID;
+    eflags_write( new_eflg );
+    new_eflg = eflags_read();
+    return( new_eflg != old_eflg );
 }
 
 int main( int argc, char **argv )
