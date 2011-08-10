00ff1de192686dd20dd613e9e77457d27aa228e4
Michal Necasek <N/A>
2011-08-10 14:40:01 +0100

    ChangeList: The C compiler now allows _Bool to integer assignments in C99 mode.
    Implementation note: This was somehow forgotten, all that was needed was editing
    the table of allowed conversions. See also Bug 987.
    
    [git-p4: depot-paths = "//depot/openwatcom/": change = 37105]

--- bld/ctest/regress/positive/source/bool02.c
+++ bld/ctest/regress/positive/source/bool02.c
@@ -1,9 +1,10 @@
-/* 
- * Test of conversion from / to _Bool bit-field
- */
-
 #include "fail.h"
 #include <stdbool.h>
+
+/* 
+ * Test of conversion from / to _Bool bit-fields. Note that this test is written
+ * such that it will fail unless it's built with -za99.
+ */
 
 #define TRUE    1
 #define FALSE   0
@@ -18,12 +19,30 @@ struct bit2 {
             bool bobx : 2;
 } bits = { -1, -1, -1, -1, -1 };
 
-int main( void ){
-    bool    bol;
-    double  d;
+struct bit3 {
+    bool    b1;
+    bool    b2  : 1;
+    bool    b3  : 1;
+    int     ibf : 1;
+} bitz = { true, true, false, -1 };
+
+int main( void ) 
+{
+    bool                bol;
+    double              d;
+    signed long long    slli;
+    unsigned long long  ulli;
+    signed long         sli;
+    unsigned long       uli;
+    signed              si;
+    unsigned            ui;
+    signed char         sc;
+    unsigned char       uc;
+    char                c;
 
     if( func_bol( FALSE ) == TRUE ) fail( __LINE__ );
     if( func_bol( TRUE ) != TRUE ) fail( __LINE__ );
+    if( func_bol( -3 ) != TRUE ) fail( __LINE__ );
     if( func_bol( bits.uibf ) != TRUE ) fail( __LINE__ );
     if( func_bol( bits.sibf ) != TRUE ) fail( __LINE__ );
     if( func_bol( bits.pibf ) != TRUE ) fail( __LINE__ );
@@ -49,5 +68,94 @@ int main( void ){
     if( func_bol( bits.pibf ) != TRUE ) fail( __LINE__ );
     if( func_bol( bits.bobf ) != TRUE ) fail( __LINE__ );
     if( func_bol( bits.bobx ) != TRUE ) fail( __LINE__ );
+
+    if( bitz.ibf != -1 ) fail( __LINE__ );
+    if( bitz.b2  != 1 ) fail( __LINE__ );
+    if( bitz.b2 == bitz.ibf ) fail( __LINE__ );
+
+    slli = (  signed long long int)(bool)1u;
+    ulli = (unsigned long long int)(bool)1u;
+    if( slli != ulli ) fail( __LINE__ );
+    slli = (  signed long long int)bitz.b1;
+    ulli = (unsigned long long int)bitz.b1;
+    if( slli != ulli ) fail( __LINE__ );
+    slli = (  signed long long int)bitz.b2;
+    ulli = (unsigned long long int)bitz.b2;
+    if( slli != ulli ) fail( __LINE__ );
+    slli = bitz.b2;
+    ulli = bitz.b2;
+    if( slli != ulli ) fail( __LINE__ );
+    slli = bitz.b3;
+    if( slli == ulli ) fail( __LINE__ );
+
+    sli = (  signed long int)(bool)1u;
+    uli = (unsigned long int)(bool)1u;
+    if( sli != uli ) fail( __LINE__ );
+    sli = (  signed long int)bitz.b1;
+    uli = (unsigned long int)bitz.b1;
+    if( sli != uli ) fail( __LINE__ );
+    sli = (  signed long int)bitz.b2;
+    uli = (unsigned long int)bitz.b2;
+    if( sli != uli ) fail( __LINE__ );
+    sli = bitz.b2;    
+    uli = bitz.b2;
+    if( sli != uli ) fail( __LINE__ );
+    sli = bitz.b3;
+    if( sli == uli ) fail( __LINE__ );
+
+    si = (  signed int)(bool)1u;
+    ui = (unsigned int)(bool)1u;
+    if( si != ui ) fail( __LINE__ );
+    si = (  signed int)bitz.b1;
+    ui = (unsigned int)bitz.b1;
+    if( si != ui ) fail( __LINE__ );
+    si = (  signed int)bitz.b2;
+    ui = (unsigned int)bitz.b2;
+    if( si != ui ) fail( __LINE__ );
+    si = bitz.b2;    
+    ui = bitz.b2;
+    if( si != ui ) fail( __LINE__ );
+    ui = bitz.b3;
+    if( si == ui ) fail( __LINE__ );
+
+    sc = (  signed char)(bool)1u;
+    uc = (unsigned char)(bool)1u;
+    if( sc != uc ) fail( __LINE__ );
+    sc = (  signed char)bitz.b1;
+    uc = (unsigned char)bitz.b1;
+    if( sc != uc ) fail( __LINE__ );
+    sc = (  signed char)bitz.b2;
+    uc = (unsigned char)bitz.b2;
+    if( sc != uc ) fail( __LINE__ );
+    sc = bitz.b2;    
+    uc = bitz.b2;
+    if( sc != uc ) fail( __LINE__ );
+    uc = bitz.b3;
+    if( sc == uc ) fail( __LINE__ );
+
+    sc = (  signed char)(bool)1u;
+    uc = (unsigned char)(bool)1u;
+     c = (         char)(bool)1u;
+    if( sc != c ) fail( __LINE__ );
+    if( uc != c ) fail( __LINE__ );
+    sc = (  signed char)bitz.b1;
+    uc = (unsigned char)bitz.b1;
+     c = (         char)bitz.b1;
+    if( sc != c ) fail( __LINE__ );
+    if( uc != c ) fail( __LINE__ );
+    sc = (  signed char)bitz.b2;
+    uc = (unsigned char)bitz.b2;
+     c = (         char)bitz.b2;
+    if( sc != c ) fail( __LINE__ );
+    if( uc != c ) fail( __LINE__ );
+    sc = bitz.b2;    
+    uc = bitz.b2;
+     c = bitz.b2;
+    if( sc != c ) fail( __LINE__ );
+    if( uc != c ) fail( __LINE__ );
+     c = bitz.b3;
+    if( c == uc ) fail( __LINE__ );
+    if( c == sc ) fail( __LINE__ );
+
     _PASS;
 }
