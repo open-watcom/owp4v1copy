7100f2248273b3c59f9b8229bca9746fb5c5e143
Paul S. Person <N/A>
2015-07-10 00:52:05 +0100

    to see if helps reported build failure
    [git-p4: depot-paths = "//depot/openwatcom/": change = 37630]

--- bld/wgml/c/gsbdbius.c
+++ bld/wgml/c/gsbdbius.c
@@ -24,13 +24,80 @@
 *
 *  ========================================================================
 *
-* Description:  WGML implement .us script control word
-*                    only line option implemented
+* Description:  WGML implementation of .bd, .bi, and .us script control
+*                    words
+*                   interactions are only partially understood/implemented
 *
 ****************************************************************************/
 #include    "wgml.h"
 #include    "gvars.h"
 
+
+/**************************************************************************/
+/* BOLD boldfaces the specified number of input text lines.               */
+/*                                                                        */
+/*      旼컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴커      */
+/*      |       |                                                  |      */
+/*      |  .BD  |    <1|n|ON|OFF|line>                             |      */
+/*      |       |                                                  |      */
+/*      읕컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴켸      */
+/*                                                                        */
+/*                                                                        */
+/* n:   The next "n" input text lines are to be made Bold by overstriking */
+/*      each character with itself.  If "n" is omitted,  a value  of 1 is */
+/*      assumed.                                                          */
+/* ON:  All following input text lines are to be made Bold.               */
+/* OFF: Terminates  the boldface  effect,  whether  initiated by  "n" or  */
+/*      "ON".                                                             */
+/* line: The  boldface effect will be  applied to the  resulting "line".  */
+/*    If "line"  starts with a control  word indicator,  the  Bold action */
+/*    will start one blank after the control word at the start of "line", */
+/*    and the result of overstriking must  fit within the maximum allowed */
+/*    input line  length.   This restriction does  not apply to  a "line" */
+/*    operand of text or text following a numeric or "ON" operand.        */
+/*                                                                        */
+/* This control word does not cause a break.  BOLD operates independently */
+/* of other control words that modify text.    When more than one of .BD, */
+/* .BI,  .UC,  .UL,  .UP,  or .US are  in effect,  the result is the best */
+/* equivalent of the  sum of the effects.    They may be disabled  in any */
+/* order;  the result  will be the best  equivalent of the ones  still in */
+/* effect.   The .UD "INCLUDE/IGNORE/SET" characters  may be used to turn */
+/* the function on and  off within a "line" or in  subsequent input text. */
+/* See .UD for defaults and details.                                      */
+/*                                                                        */
+/* NOTES                                                                  */
+/* (1) For output devices  that support multiple fonts,   the output from */
+/*     this  control word  is  not necessarily  bold  but  may merely  be */
+/*     selected from a different font.                                    */
+/* (2) Text may also be emphasized with the &'bold( function.             */
+/* (3) Do not  attempt to use a  numeric expression as a  "line" operand, */
+/*     because  it  will be  taken  as  an  "n" operand.    For  example, */
+/*     ".BD (1988)" will boldface  the next 1988 input lines  of text and */
+/*     not the string  "(1988)".   Use ".BD 1;(1988)" or  ".BD;(1988)" or */
+/*     &'bold(1988) instead.                                              */
+/*                                                                        */
+/**************************************************************************/
+
+/**************************************************************************/
+/* BOLD ITALIC overstrikes an input line with itself and underscores it   */
+/* for a boldface italic effect.                                          */
+/*                                                                        */
+/*      旼컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴커      */
+/*      |       |                                                  |      */
+/*      |  .BI  |    <1|n|ON|OFF|line>                             |      */
+/*      |       |                                                  |      */
+/*      읕컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴켸      */
+/*                                                                        */
+/* This control word does not cause a break. The operands are identical   */
+/* to those for the .BD control word.                                     */
+/*                                                                        */
+/* NOTES                                                                  */
+/* (1) For output devices  that support multiple fonts,   the output from */
+/*     this control word is not necessarily underscored.   It often will  */
+/*     display in a bold italic font, for example.                        */
+/* (2) Text may also be emphasized with the &'bitalic( function.          */
+/*                                                                        */
+/**************************************************************************/
 
 /**************************************************************************/
 /* UNDERSCORE underscores text in the specified number of input lines.    */
@@ -51,47 +118,218 @@
 /*     display in an italic font, for example.                            */
 /* (2) Text may also be emphasized with the &'italic( function.           */
 /*                                                                        */
-/* EXAMPLES                                                               */
-/* (1) This is example number                                             */
-/*     .us one.                                                           */
-/*     produces:  This is example number one.                             */
-/* (2) 1.2 &'italic('Chapter Title')                                      */
-/*     produces:  1.2 Chapter Title                                       */
-/* (3) .us on                                                             */
-/*     This is a simple test of underscoring some                         */
-/*     input text.                                                        */
-/*     .ud on 40                                                          */
-/*     And now we will underscore the interword blanks,                   */
-/*     but justification can continue.                                    */
-/*     Since the period is not underscored there                          */
-/*     are still blanks between sentences.                                */
-/*     .ud on . ?                                                         */
-/*     Now even the sentences will be joined because                      */
-/*     the special characters period and question mark                    */
-/*     are also underscored.                                              */
-/*     How does it look?                                                  */
-/*          Note that leading blanks on a line are not                    */
-/*     underscored.                                                       */
-/*     The end.                                                           */
-/*     .us off                                                            */
-/*     produces:   This is a simple test of underscoring some input text. */
-/*     And now we will underscore the interword blanks, but justification */
-/*     can continue.  Since the period is not underscored there are still */
-/*     blanks between sentences.   Now even  the sentences will be joined */
-/*     because the special  characters period and question  mark are also */
-/*     underscored.  How does it look?                                    */
-/*          Note that leading blanks on a line are not underscored.   The */
-/*     end.                                                               */
 /**************************************************************************/
 
-void scr_us( void )
-{
-    char    *   p = scan_start + 1;
 
-    if( *p ) {
-        process_text( p, 1 );           // hilite the remaining text
+/**************************************************************************/
+/* The common function does all the work; the sc_xx functions specify     */
+/* which control word is being processed                                  */
+/* NOTE: the treatment of BI in wgml 4.0 is quite different from what     */
+/*       might be expected from the way BI is documented; this attempts   */
+/*       to follow a reasonable interpretation of the documentation       */
+/*       by treating BI as a quick way to set or clear both BD and US     */
+/**************************************************************************/
+
+static void scr_style_common( style_cw_type type, style_cw_info * cw_info )
+{
+    char            *   p;
+    char            *   pa;
+    condcode            cc;
+    getnum_block        gn;
+    int                 len;
+
+    if( script_style.style == SCT_none ) {  // save current font if no script style
+        script_style.font = g_curr_font;
     }
+
+    garginit();         // find end of control word
+
+    p = scan_start;     // for use later
+    cc = getarg();      // sets token_start, scan_start and scan_end
+
+    if( cc == omit ) {                  // same as BD 1
+        script_style.style |= type;
+        cw_info->count = 1;
+        cw_info->scope = SCS_count;
+        pa = scan_start;                // nothing following
+    } else {                            // "pos", "quotes", "quote0"
+        while( *p && *p == ' ' ) {      // first token start
+            p++;
+        }
+        pa = p;
+        while( *p && *p != ' ' ) {      // end of token 
+            p++;
+        }
+        len = p - pa;
+
+        while( *p && *p == ' ' ) {      // second token?
+            p++;
+        }
+        if( !*p ) {                     // no second token
+            if( (len == 3) && (!strnicmp( pa, "OFF", len )) ) { // OFF
+                if( type == SCT_bi ) {
+                    script_style.style &= ~SCT_bd;
+                    script_style.cw_bd.count = 0;
+                    script_style.cw_bd.scope = SCS_none;
+                    script_style.style &= ~SCT_us;
+                    script_style.cw_us.count = 0;
+                    script_style.cw_us.scope = SCS_none;
+                } else {
+                    script_style.style &= ~type;
+                    cw_info->count = 0;
+                    cw_info->scope = SCS_none;
+                }
+                pa = p;                         // no following text
+            } else if( (len == 2) && (!strnicmp( pa, "ON", len )) ) {  // ON
+                if( type == SCT_bi ) {
+                    script_style.style |= SCT_bd;
+                    script_style.cw_bd.count = 0;
+                    script_style.cw_bd.scope = SCS_on;
+                    script_style.style |= SCT_us;
+                    script_style.cw_us.count = 0;
+                    script_style.cw_us.scope = SCS_on;
+                } else {
+                    script_style.style |= type;
+                    cw_info->count = 0;
+                    cw_info->scope = SCS_on;
+                }
+                pa = p;
+            } else {                            // no following text
+                gn.argstart      = tok_start;
+                gn.argstop       = scan_stop;
+                gn.ignore_blanks = 0;
+
+                cc = getnum( &gn );             // try numeric expression evaluation
+                if( (cc == notnum) || (cc == neg) ) {
+                    p = pa;                     // output as text
+                } else if( gn.result == 0 ) {
+                    pa = p;                     // nothing happens at all
+                } else {                        // number > 0
+                    if( type == SCT_bi ) {
+                        script_style.style |= SCT_bd;
+                        script_style.cw_bd.count = gn.result;
+                        script_style.cw_bd.scope = SCS_count;
+                        script_style.style |= SCT_us;
+                        script_style.cw_us.count = gn.result;
+                        script_style.cw_us.scope = SCS_count;
+                    } else {
+                        script_style.style |= type;
+                        cw_info->count = gn.result;
+                        cw_info->scope = SCS_count;
+                    }
+                    pa = p;                     // no following text
+                }
+            }
+        }
+    }
+
+    if( *pa ) {
+        if( type == SCT_bi ) {
+            script_style.style |= SCT_bd;
+            script_style.cw_bd.count = 0;
+            script_style.cw_bd.scope = SCS_line;
+            script_style.style |= SCT_us;
+            script_style.cw_us.count = 0;
+            script_style.cw_us.scope = SCS_line;
+        } else {
+            script_style.style |= type;
+            cw_info->count = 0;
+            cw_info->scope = SCS_line;
+        }
+        process_text( pa, g_curr_font );     // submit text on the same line
+        if( type == SCT_bi ) {
+            script_style.style &= ~SCT_bd;
+            script_style.cw_bd.scope = SCS_none ;
+            script_style.style &= ~SCT_us;
+            script_style.cw_us.scope = SCS_none;
+        } else {
+            script_style.style &= ~type;
+            cw_info->scope = SCS_none;
+        }
+        g_curr_font = script_style.font;
+    }
+
     scan_restart = scan_stop + 1;
+    return;
+    
+}
+
+void scr_bd( void )
+{
+    scr_style_common( SCT_bd, &script_style.cw_bd );
     return;
 }
 
+void scr_bi( void )
+{
+    scr_style_common( SCT_bi, NULL );   // BI is handled in a special manner
+    return;
+}
+
+void scr_us( void )
+{
+    scr_style_common( SCT_us, &script_style.cw_us );
+    return;
+}
+
+/************************************************************************/
+/* scr_style_end() terminates all processing controlled by the control  */
+/*   words defined in this file                                         */
+/* for example, the next tag will cause this function to be called      */
+/************************************************************************/
+
+void scr_style_end( void )
+{
+    script_style.style = SCT_none;
+    script_style.cw_bd.count = 0;
+    script_style.cw_bd.scope = SCS_none;
+    script_style.cw_us.count = 0;
+    script_style.cw_us.scope = SCS_none;
+
+    g_curr_font = script_style.font;
+
+    return;
+}
+
+/************************************************************************/
+/* scr_style_font() adjusts script_style for a new input line and       */
+/*   returns the resulting font                                         */
+/************************************************************************/
+
+font_number scr_style_font( font_number in_font )
+{
+    font_number     font;
+
+    if( input_cbs->fmflags & II_sol ) {             // only adjust for SOL
+        if( script_style.cw_bd.scope == SCS_count ) {   // adjust/remove BD if appropriate
+            if( script_style.cw_bd.count > 0 ) {
+                script_style.cw_bd.count --;
+            } else {
+                script_style.cw_bd.scope = SCS_none;
+                script_style.style &= ~SCT_bd;
+            }
+        }
+
+        if( script_style.cw_us.scope == SCS_count ) {   // adjust/remove US if appropriate
+            if( script_style.cw_us.count > 0 ) {
+                script_style.cw_us.count --;
+            } else {
+                script_style.cw_us.scope = SCS_none;
+                script_style.style &= ~SCT_us;
+            }
+        }
+    }
+
+    if( (script_style.style & SCT_bd) && (script_style.style & SCT_us) ) {
+        font = 3;
+    } else if( script_style.style & SCT_bd ) {
+        font = 2;
+    } else if( script_style.style & SCT_us ) {
+        font = 1;
+    } else {                                // no script_style
+        font = in_font;                     // TBD
+        g_curr_font = script_style.font;    // restore current font
+    }
+
+    return( font );
+}    
