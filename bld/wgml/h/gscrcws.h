7bd6b9e806e0866c3fad5d08360c47fa08b78cdd
Paul S. Person <N/A>
2017-09-28 17:50:04 +0100

    almost implements NL, and /internally generated/ control word one of our docs uses explicitly which basically does ... nothing.
    [git-p4: depot-paths = "//depot/openwatcom/": change = 37790]

--- bld/wgml/h/gscrcws.h
+++ bld/wgml/h/gscrcws.h
@@ -112,7 +112,7 @@
   picks( mc, scr_dummy, cw_break+cw_o_t    )// multiple column
   picks( me, scr_me,    0                  )// macro exit
   picks( ms, scr_dummy, 0                  )// macro substitution
-  picks( nl, scr_dummy, 0                  )// null line
+  picks( nl, scr_nl,    0                  )// null line
   picks( oc, scr_oc,    0                  )// output comment
   picks( of, scr_dummy, cw_break+cw_o_t    )// offset
   picks( oj, scr_dummy, cw_break+cw_o_t    )// out justify
